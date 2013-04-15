0827e671747ba07026334d8128b33fbe8e39aed3
Kei Hibino <ex8k.hibino@gmail.com>
2013-04-15 18:52:16 +0900

    To remove dependency on toSql and fromSql combinators of HDBC. Persistable value is not record, so composed type is not overlap.

--- schema-th/src/Database/HDBC/Record/Persistable.hs
+++ schema-th/src/Database/HDBC/Record/Persistable.hs
@@ -1,16 +1,18 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Database.HDBC.Record.Persistable (
   persistableSingleton
   ) where
 
 import Database.Record.Persistable
-  (Singleton, singleton, runSingleton,
+  (Singleton,
    persistableNullValue, PersistableNull (..),
    PersistableSqlValue, -- persistableSqlValue,
-   PersistableRecord, persistableRecordFromValue,
+   PersistableRecord, persistableSingletonFromValue,
    PersistableValue (persistableValue),
    Persistable (persistable))
 import qualified Database.Record.Persistable as Persistable
@@ -19,18 +21,18 @@ import Data.Convertible (Convertible)
 import Database.HDBC (SqlValue(SqlNull), fromSql, toSql)
 
 persistableSqlValue :: (Convertible SqlValue a, Convertible a SqlValue)
-                       => PersistableSqlValue SqlValue (Singleton a)
-persistableSqlValue =  Persistable.persistableSqlValue (singleton . fromSql) (toSql . runSingleton)
+                       => PersistableSqlValue SqlValue a
+persistableSqlValue =  Persistable.persistableSqlValue fromSql toSql
 
 persistableSingleton :: (Convertible SqlValue a, Convertible a SqlValue)
                      => PersistableRecord SqlValue (Singleton a)
-persistableSingleton =  persistableRecordFromValue persistableSqlValue
+persistableSingleton =  persistableSingletonFromValue persistableSqlValue
 
 instance PersistableNull SqlValue  where
   persistableNull = persistableNullValue SqlNull
 
 instance (Convertible SqlValue a, Convertible a SqlValue)
-         => PersistableValue SqlValue (Singleton a)  where
+         => PersistableValue SqlValue a  where
   persistableValue = persistableSqlValue
 
 instance (Convertible SqlValue a, Convertible a SqlValue)
--- schema-th/src/Database/HDBC/TH.hs
+++ schema-th/src/Database/HDBC/TH.hs
@@ -55,7 +55,7 @@ import Data.Char (toUpper, toLower)
 import Data.Maybe (fromJust, listToMaybe)
 import Data.List (elemIndex)
 
-import Database.HDBC (IConnection, SqlValue, fromSql, toSql)
+import Database.HDBC (IConnection, SqlValue)
 
 import Language.Haskell.TH
   (Q, Name, mkName, runQ, runIO, Ppr, ppr,
@@ -71,7 +71,8 @@ import qualified Language.Haskell.TH.Syntax as TH
 
 import Database.HDBC.Session (withConnectionIO)
 import Database.Record.Persistable
-  (persistableRecord, Persistable, persistable, Singleton)
+  (persistableRecord, Persistable, persistable,
+   fromSql, toSql, Singleton)
 import Database.Record.KeyConstraint
   (HasKeyConstraint(constraintKey), specifyKeyConstraint, Primary, NotNull)
 import Database.Record.FromSql (FromSql(recordFromSql), recordFromSql')
