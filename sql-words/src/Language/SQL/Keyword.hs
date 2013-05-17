a7e1778b31277ec3121f3822d387a944fdb5e644
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-17 18:29:50 +0900

    Add integrated module.

---
+++ sql-words/src/Language/SQL/Keyword.hs
@@ -0,0 +1,17 @@
+-- |
+-- Module      : Language.SQL.Keyword
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- SQL keyword representation using Haskell data constructors.
+module Language.SQL.Keyword (
+  module Language.SQL.Keyword.Type,
+  module Language.SQL.Keyword.Concat
+  ) where
+
+import Language.SQL.Keyword.Type
+import Language.SQL.Keyword.Concat
