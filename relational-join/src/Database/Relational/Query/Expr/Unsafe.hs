9b44ed62800edb978426c0815e8d45325a7b389f
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-21 11:19:14 +0900

    Remove old unused codes.

--- relational-join/src/Database/Relational/Query/Expr/Unsafe.hs
+++ relational-join/src/Database/Relational/Query/Expr/Unsafe.hs
@@ -1,31 +1,9 @@
 
 module Database.Relational.Query.Expr.Unsafe (
   Expr(Expr, showExpr),
-
-  compareBinOp, numBinOp, binOp
   ) where
-
-import qualified Language.SQL.Keyword as SQL
 
 newtype Expr a = Expr { showExpr :: String }
 
 instance Show (Expr a) where
   show = showExpr
-
-paren :: String -> String
-paren = ('(' :) . (++[')'])
-
-binOp :: (SQL.Keyword -> SQL.Keyword -> SQL.Keyword)
-      -> Expr ft -> Expr ft -> Expr a
-binOp op a b = Expr . paren . SQL.wordShow
-               $ op (wordExpr a) (wordExpr b)
-  where wordExpr = SQL.word . showExpr
-
-compareBinOp :: (SQL.Keyword -> SQL.Keyword -> SQL.Keyword)
-             -> Expr ft -> Expr ft -> Expr Bool
-compareBinOp =  binOp
-
-numBinOp :: Num a
-         => (SQL.Keyword -> SQL.Keyword -> SQL.Keyword)
-         -> Expr a -> Expr a -> Expr a
-numBinOp =  binOp
