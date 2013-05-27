62c47abe8170dfc6e3bfa0e97e23dd51ff5914bb
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-27 22:16:21 +0900

    Add restriction specifier for direct join operators.

--- relational-join/src/Database/Relational/Query/Relation.hs
+++ relational-join/src/Database/Relational/Query/Relation.hs
@@ -10,6 +10,7 @@ module Database.Relational.Query.Relation (
 
   inner', left', right', full',
   inner, left, right, full,
+  on',
 
   sqlFromRelation,
 
@@ -18,7 +19,7 @@ module Database.Relational.Query.Relation (
   nested, width
   ) where
 
-import Database.Relational.Query.Monad.Class (MonadQuery)
+import Database.Relational.Query.Monad.Class (MonadQuery (on))
 import qualified Database.Relational.Query.Monad.Unsafe as UnsafeMonadQuery
 import Database.Relational.Query.Monad.Simple (QuerySimple, SimpleQuery)
 import qualified Database.Relational.Query.Monad.Simple as Simple
@@ -29,6 +30,7 @@ import Database.Relational.Query.Table (Table)
 
 import Database.Relational.Query.Internal.Product (NodeAttr(Just', Maybe))
 
+import Database.Relational.Query.Expr (Expr)
 import Database.Relational.Query.Projection (Projection)
 import qualified Database.Relational.Query.Projection as Projection
 import Database.Relational.Query.Aggregation (Aggregation)
@@ -93,38 +95,46 @@ aggregateRelation' :: QueryAggregate (PlaceHolders p, Aggregation r) -> PrimeRel
 aggregateRelation' =  AggregateRel . fmap snd
 
 
+type JoinRestriction a b = (Projection a -> Projection b -> Expr (Maybe Bool))
+
 join' :: ProjectableGeneralizedZip pa pb pc
       => (qa -> QuerySimple (PlaceHolders pa, Projection a))
       -> (qb -> QuerySimple (PlaceHolders pb, Projection b))
       -> qa
       -> qb
+      -> [JoinRestriction a b]
       -> PrimeRelation pc (a, b)
-join' qL qR r0 r1 = relation' $ do
+join' qL qR r0 r1 ons = relation' $ do
   (ph0, pj0) <- qL r0
   (ph1, pj1) <- qR r1
+  sequence_ $ zipWith3 (\f a b -> on $ f a b) ons (repeat pj0) (repeat pj1)
   return $ (ph0 `generalizedZip` ph1, pj0 `projectZip` pj1)
 
 inner' :: ProjectableGeneralizedZip pa pb pc
        => PrimeRelation pa a
        -> PrimeRelation pb b
+       -> [JoinRestriction a b]
        -> PrimeRelation pc (a, b)
 inner' =  join' query' query'
 
 left'  :: ProjectableGeneralizedZip pa pb pc
        => PrimeRelation pa a
        -> PrimeRelation pb b
+       -> [JoinRestriction a (Maybe b)]
        -> PrimeRelation pc (a, Maybe b)
 left'  =  join' query' queryMaybe'
 
 right' :: ProjectableGeneralizedZip pa pb pc
        => PrimeRelation pa a
        -> PrimeRelation pb b
+       -> [JoinRestriction (Maybe a) b]
        -> PrimeRelation pc(Maybe a, b)
 right' =  join' queryMaybe' query'
 
 full'  :: ProjectableGeneralizedZip pa pb pc
        => PrimeRelation pa a
        -> PrimeRelation pb b
+       -> [JoinRestriction (Maybe a) (Maybe b)]
        -> PrimeRelation pc (Maybe a, Maybe b)
 full'  =  join' queryMaybe' queryMaybe'
 
@@ -132,33 +142,44 @@ join :: (qa -> QuerySimple (Projection a))
      -> (qb -> QuerySimple (Projection b))
      -> qa
      -> qb
+     -> [JoinRestriction a b]
      -> Relation (a, b)
-join qL qR r0 r1 = relation $ do
+join qL qR r0 r1 ons = relation $ do
   pj0 <- qL r0
   pj1 <- qR r1
+  sequence_ $ zipWith3 (\f a b -> on $ f a b) ons (repeat pj0) (repeat pj1)
   return $ pj0 `projectZip` pj1
 
 inner :: Relation a
       -> Relation b
+      -> [JoinRestriction a b]
       -> Relation (a, b)
 inner =  join query query
 
 left  :: Relation a
       -> Relation b
+      -> [JoinRestriction a (Maybe b)]
       -> Relation (a, Maybe b)
 left  =  join query queryMaybe
 
 right :: Relation a
       -> Relation b
+      -> [JoinRestriction (Maybe a) b]
       -> Relation (Maybe a, b)
 right =  join queryMaybe query
 
 full  :: Relation a
       -> Relation b
+      -> [JoinRestriction (Maybe a) (Maybe b)]
       -> Relation (Maybe a, Maybe b)
 full  =  join queryMaybe queryMaybe
 
-infix 8 `inner'`, `left'`, `right'`, `full'`, `inner`, `left`, `right`, `full`
+on' :: ([JoinRestriction a b] -> PrimeRelation pc (a, b))
+    -> [JoinRestriction a b]
+    -> PrimeRelation pc (a, b)
+on' =  ($)
+
+infixl 8 `inner'`, `left'`, `right'`, `full'`, `inner`, `left`, `right`, `full`, `on'`
 
 
 sqlFromRelation :: PrimeRelation p r -> String
