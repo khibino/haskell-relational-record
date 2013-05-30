4338e2dfaa541e66480f608a7fbe7dd9987f79b6
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-30 19:02:56 +0900

    Update haddock.

--- sql-words/src/Language/SQL/Keyword/Type.hs
+++ sql-words/src/Language/SQL/Keyword/Type.hs
@@ -19,11 +19,11 @@ module Language.SQL.Keyword.Type (
   stringMap
   ) where
 
-import Prelude hiding (and, or)
 import Data.String (IsString(fromString))
 import Data.List (find)
 
 
+-- | Type represent SQL keywords.
 data Keyword = SELECT | ALL | DISTINCT | ON
              | GROUP | COUNT | SUM | AVG | MAX | MIN | EVERY | ANY | SOME
              | ORDER | BY | ASC | DESC | HAVING
@@ -43,39 +43,52 @@ data Keyword = SELECT | ALL | DISTINCT | ON
              | CASE | END | WHEN | ELSE | THEN
 
              | LIKE
-             -- | (:?)
-             -- | (:+) | (:-) | (:*) | (:/)
              | AND | OR | NOT
 
              | IS | NULL | IN
 
-             -- | OPEN | CLOSE
-
              | Sequence String
              deriving (Read, Show)
 
+             {-
+                  | (:?)
+                  | (:+) | (:-) | (:*) | (:/)
+                  | OPEN | CLOSE
+             -}
 
+
+-- | 'Keyword' type with OverloadedString extension,
+--   can be involved same list with string literals.
+--
+-- > selectFoo = [SELECT, "a, b, c", FROM, "foo"]
+--
 instance IsString Keyword where
   fromString s' = found (find ((== "") . snd) (reads s')) s'  where
    found  Nothing      s = Sequence s
    found (Just (w, _)) _ = w
 
+-- | Make 'Keyword' from String
 word :: String -> Keyword
 word =  Sequence
 
+-- | Show 'Keyword'
 wordShow :: Keyword -> String
 wordShow =  d  where
   d (Sequence s)   = s
   d w              = show w
 
+-- | Concatinate keywords into 'String' like unwords
 unwordsSQL :: [Keyword] -> String
 unwordsSQL =  unwords . map wordShow
 
+-- | Make SQL string expression. No escape logic, so this is unsafe function.
 string :: String -> Keyword
 string =  Sequence . ('\'' :) . (++ "'")
 
+-- | Make SQL integer expression
 integer :: (Integral a, Show a) => a -> Keyword
 integer =  Sequence . show
 
+-- | Map 'String' function into 'Keyword' function
 stringMap :: (String -> String) -> Keyword -> Keyword
 stringMap f = word . f . wordShow
