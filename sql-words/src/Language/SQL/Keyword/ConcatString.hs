5b255fddfbd99d402c41e58ba1af78d3465ac9b1
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-30 20:56:58 +0900

    Update haddock.

--- sql-words/src/Language/SQL/Keyword/ConcatString.hs
+++ sql-words/src/Language/SQL/Keyword/ConcatString.hs
@@ -11,7 +11,12 @@
 --
 --  'String' type version of "Language.SQL.Keyword.Concat"
 module Language.SQL.Keyword.ConcatString (
-  sepBy, defineBinOp,
+  -- * List concatination functions
+  sepBy,
+
+  -- * Binary operators
+  -- $binaryOperators
+  defineBinOp,
 
   as,
 
@@ -27,27 +32,62 @@ import Data.List (intersperse)
 import Language.SQL.Keyword.Type (Keyword (..), wordShow)
 
 
+-- | Separate 'String' list with delimiter 'Keyword' and map to 'String' list.
 sepBy :: [String] -> Keyword -> [String]
 ws `sepBy` d = intersperse (wordShow d) $ ws
 
+{- $binaryOperators
+Binary operators on SQL. Result is concatinated into one 'String'.
+-}
+
+-- | Define binary operator on 'String' type represeted by specified 'Keyword'.
+--   Result is delimited by whitespace like unwords on 'String' list.
 defineBinOp :: Keyword -> String -> String -> String
 defineBinOp op a b = unwords $ [a, b] `sepBy` op
 
-
+-- | Binary operator for SQL string expression concatination.
+(.||.) :: String -> String -> String
 (.||.) =  defineBinOp "||"
-(.=.)  =  defineBinOp "="
-(.<.)  =  defineBinOp "<"
-(.<=.) =  defineBinOp "<="
-(.>.)  =  defineBinOp ">"
-(.>=.) =  defineBinOp ">="
-(.<>.) =  defineBinOp "<>"
-as     =  defineBinOp AS
-and    =  defineBinOp AND
-or     =  defineBinOp OR
-in'    =  defineBinOp IN
 
-(.||.), (.=.), (.<.), (.<=.), (.>.), (.>=.), (.<>.), as, and, or, in'
-  :: String -> String -> String
+-- | Binary eq operator for SQL expression.
+(.=.) :: String -> String -> String
+(.=.)  =  defineBinOp "="
+
+-- | Binary not eq operator for SQL expression.
+(.<>.) :: String -> String -> String
+(.<>.) =  defineBinOp "<>"
+
+-- | Binary lt operator for SQL expression.
+(.<.) :: String -> String -> String
+(.<.)  =  defineBinOp "<"
+
+-- | Binary le operator for SQL expression.
+(.<=.) :: String -> String -> String
+(.<=.) =  defineBinOp "<="
+
+-- | Binary gt operator for SQL expression.
+(.>.) :: String -> String -> String
+(.>.)  =  defineBinOp ">"
+
+-- | Binary ge operator for SQL expression.
+(.>=.) :: String -> String -> String
+(.>=.) =  defineBinOp ">="
+
+-- | Binary operator for SQL name alias.
+as :: String -> String -> String
+as     =  defineBinOp AS
+
+-- | Binary `AND` operator for SQL boolean expression.
+and :: String -> String -> String
+and    =  defineBinOp AND
+
+-- | Binary `OR` operator for SQL boolean expression.
+or :: String -> String -> String
+or     =  defineBinOp OR
+
+-- | Binary `IN` operator for SQL.
+in' :: String -> String -> String
+in'    =  defineBinOp IN
 
 infixr 5 .||.
 infixr 4 .=., .<., .<=., .>., .>=., .<>.
