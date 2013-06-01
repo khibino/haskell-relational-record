447509d120a0c4c0e5b9f0009d867a34bf4a01bb
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-02 00:26:14 +0900

    Add outline document for integrated module.

--- DB-record/src/Database/Record.hs
+++ DB-record/src/Database/Record.hs
@@ -1,9 +1,36 @@
+-- |
+-- Module      : Database.Record
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This is integrated module which contains
+-- types to represent table constraints and
+-- interfaces to bind between SQL database values and Haskell records.
 module Database.Record (
+  -- * Binding between SQL database values and Haskell records
+  -- $bindSqlAndHaskellRecords
+  -- * Constraints used by derivation rules
+  -- $constraintsForDerivation
+  -- * Modules which provide proof objects
+  -- ** Table constraint
   module Database.Record.KeyConstraint,
+  -- ** Convert between Haskell type and list of SQL type
   module Database.Record.Persistable,
+  -- ** Convert from list of SQL type
   module Database.Record.FromSql,
+  -- ** Convert into list of SQL type
   module Database.Record.ToSql
   ) where
+
+{- $bindSqlAndHaskellRecords
+-}
+
+{- $constraintsForDerivation
+-}
 
 import Database.Record.KeyConstraint
   (KeyConstraint, HasKeyConstraint(..),
