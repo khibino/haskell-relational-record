dfa4ef1542a7318830ff8135a2b9ccee1395be89
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-02 01:08:54 +0900

    Update docuemnts of proof objects for convertion from SQL type.

--- DB-record/src/Database/Record/FromSql.hs
+++ DB-record/src/Database/Record/FromSql.hs
@@ -10,18 +10,23 @@
 -- Maintainer  : ex8k.hibino@gmail.com
 -- Stability   : experimental
 -- Portability : unknown
+--
+-- This module defines interfaces
+-- from list of SQL type into Haskell type.
 module Database.Record.FromSql (
+  -- * Conversion from list of SQL type into record type
+  -- $recordFromSql
   RecordFromSql, runTakeRecord, runToRecord,
   createRecordFromSql,
 
   recordDeSerializer,
 
   (<&>),
-
   maybeRecord,
 
+  -- * Inference rules of 'RecordFromSql' conversion
   FromSql (recordFromSql), recordFromSql',
-  takeRecord, toRecord,
+  takeRecord, toRecord
   ) where
 
 import Database.Record.Persistable
@@ -34,20 +39,60 @@ import Database.Record.KeyConstraint
 import Control.Monad (liftM, ap)
 import Control.Applicative ((<$>), Applicative(pure, (<*>)))
 
+{- $recordFromSql
+Structure of 'RecordFromSql' 'q' 'a' is similar to parser.
+While running 'RecordFromSql' behavior is the same as parser 
+which parse list of SQL type ['q'] stream.
 
-newtype RecordFromSql q a =
-  RecordFromSql
-  { runTakeRecord :: [q] -> (a, [q]) }
+So, 'RecordFromSql' 'q' is 'Monad' and 'Applicative' instance like parser monad.
+When, you have data constructor and objects like below.
 
+@
+  data MyRecord = MyRecord Foo Bar Baz
+@
+
+@
+  foo :: 'RecordFromSql' SqlValue Foo
+  foo =  ...
+  bar :: 'RecordFromSql' SqlValue Bar
+  bar =  ...
+  baz :: 'RecordFromSql' SqlValue Bar
+  baz =  ...
+@
+
+You can get composed 'RecordFromSql' like below.
+
+@
+  myRecord :: RecordFromSql SqlValue MyRecord
+  myRecord =  MyRecord \<$\> foo \<*\> bar \<*\> baz
+@
+-}
+
+-- | Proof object type to convert from sql value type 'q' list into Haskell type 'a'.
+newtype RecordFromSql q a = RecordFromSql ([q] -> (a, [q]))
+
+-- | Run 'RecordFromSql' proof object.
+--   Convert from list of SQL type ['q'] into Haskell type 'a' and rest of list ['q'].
+runTakeRecord :: RecordFromSql q a -> [q] -> (a, [q])
+runTakeRecord (RecordFromSql f) = f
+
+-- | Axiom of 'RecordFromSql' for SQL type 'q' and Haskell type 'a'
 createRecordFromSql :: ([q] -> (a, [q])) -> RecordFromSql q a
 createRecordFromSql =  RecordFromSql
 
+-- | Derive 'RecordFromSql' proof object from 'PersistableRecord'.
 recordDeSerializer :: PersistableRecord q a -> RecordFromSql q a
 recordDeSerializer =  createRecordFromSql . Persistable.takeRecord
 
+-- | Inferred 'RecordFromSql' proof object.
+recordFromSql' :: Persistable q a => RecordFromSql q a
+recordFromSql' =  recordDeSerializer persistable
+
+-- | Run 'RecordFromSql' proof object. Convert from list of SQL type ['q'] into  Haskell type 'a'.
 runToRecord :: RecordFromSql q a -> [q] -> a
 runToRecord r = fst . runTakeRecord r
 
+-- | 'Monad' instance like parser 'Monad'.
 instance Monad (RecordFromSql q) where
   return a = createRecordFromSql ((,) a)
   ma >>= fmb =
@@ -55,19 +100,23 @@ instance Monad (RecordFromSql q) where
     (\vals -> let (a, vals') = runTakeRecord ma vals
               in  runTakeRecord (fmb a) vals')
 
+-- | Derived 'Functor' instance from 'Monad' instance
 instance Functor (RecordFromSql q) where
   fmap = liftM
 
+-- | Derived 'Applicative' instance from 'Monad' instance
 instance Applicative (RecordFromSql q) where
   pure  = return
   (<*>) = ap
 
+-- | Derivation rule of 'RecordFromSql' proof object for Haskell tuple (,) type.
 (<&>) :: RecordFromSql q a -> RecordFromSql q b -> RecordFromSql q (a, b)
 a <&> b = (,) <$> a <*> b
 
 infixl 4 <&>
 
 
+-- | Derivation rule of 'RecordFromSql' proof object for Haskell 'Maybe' type.
 maybeRecord :: PersistableType q
             => RecordFromSql q a
             -> KeyConstraint NotNull a
@@ -79,24 +128,32 @@ maybeRecord rec pkey = createRecordFromSql mayToRec where
       (a, vals') = runTakeRecord rec vals
 
 
+-- | Inference rule interface for 'RecordFromSql' proof object.
 class FromSql q a where
   recordFromSql :: RecordFromSql q a
 
-recordFromSql' :: Persistable q a => RecordFromSql q a
-recordFromSql' =  recordDeSerializer persistable
-
+-- | Inference rule of 'RecordFromSql' proof object which can convert
+--   from list of SQL type ['q'] into Haskell tuple ('a', 'b') type.
 instance (FromSql q a, FromSql q b) => FromSql q (a, b)  where
   recordFromSql = recordFromSql <&> recordFromSql
 
+-- | Inference rule of 'RecordFromSql' proof object which can convert
+--   from list of SQL type ['q'] into Haskell 'Maybe' type.
 instance (HasKeyConstraint NotNull a, FromSql q a, PersistableType q)
          => FromSql q (Maybe a)  where
   recordFromSql = maybeRecord recordFromSql keyConstraint
 
+-- | Inference rule of 'RecordFromSql' proof object which can convert
+--   from /empty/ list of SQL type ['q'] into Haskell unit () type.
 instance FromSql q () where
   recordFromSql = recordFromSql'
 
+-- | Run inferred 'RecordFromSql' proof object.
+--   Convert from list of SQL type ['q'] into haskell type 'a' and rest of list ['q'].
 takeRecord :: FromSql q a => [q] -> (a, [q])
 takeRecord =  runTakeRecord recordFromSql
 
+-- | Run inferred 'RecordFromSql' proof object.
+--   Convert from list of SQL type ['q'] into haskell type 'a'.
 toRecord :: FromSql q a => [q] -> a
 toRecord =  runToRecord recordFromSql
