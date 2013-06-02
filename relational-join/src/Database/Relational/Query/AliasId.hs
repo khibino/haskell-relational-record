bf986b56d1eea0f15c1df0bac3276cff4fa6a065
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-03 00:46:54 +0900

    Update document for alias id.

--- relational-join/src/Database/Relational/Query/AliasId.hs
+++ relational-join/src/Database/Relational/Query/AliasId.hs
@@ -1,3 +1,13 @@
+-- |
+-- Module      : Database.Relational.Query.AliasId
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines qualifier for SQL table form.
 module Database.Relational.Query.AliasId (
   Qualified
   ) where
