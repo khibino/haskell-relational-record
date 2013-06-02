a49c71a6f10da00178d52ae975f79f0d94dde362
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-02 17:47:09 +0900

    Update haddock of internal ShowS utility.

--- relational-join/src/Database/Relational/Query/Internal/ShowS.hs
+++ relational-join/src/Database/Relational/Query/Internal/ShowS.hs
@@ -1,3 +1,14 @@
+-- |
+-- Module      : Database.Relational.Query.Internal.ShowS
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module provides SQL string concatination functions
+-- which result is ShowS differential lists.
 module Database.Relational.Query.Internal.ShowS (
   showWordsSQL, showWordSQL, showUnwords
   ) where
@@ -5,12 +16,15 @@ module Database.Relational.Query.Internal.ShowS (
 import Language.SQL.Keyword (unwordsSQL)
 import qualified Language.SQL.Keyword as SQL
 
+-- | Unwords 'SQL.Keyword' list and resturns 'ShowS'.
 showWordsSQL :: [SQL.Keyword] -> ShowS
 showWordsSQL =  showString . unwordsSQL
 
+-- | From 'SQL.Keyword' into 'ShowS'.
 showWordSQL :: SQL.Keyword -> ShowS
 showWordSQL =  showString . SQL.wordShow
 
+-- | 'ShowS' version of unwords function.
 showUnwords :: [ShowS] -> ShowS
 showUnwords =  rec  where
   rec []     = id
