9536739a438acccd3050704110d881b0fc5f04c0
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-03 15:56:42 +0900

    Add document of context for aggregated query.

--- relational-join/src/Database/Relational/Query/Internal/AggregatingContext.hs
+++ relational-join/src/Database/Relational/Query/Internal/AggregatingContext.hs
@@ -1,6 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- |
+-- Module      : Database.Relational.Query.Internal.AggregatingContext
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines context of aggregated query.
 module Database.Relational.Query.Internal.AggregatingContext (
+  -- * Context of aggregated query
   AggregatingContext,
 
   primeAggregatingContext,
@@ -21,27 +32,36 @@ import Language.SQL.Keyword (Keyword(..), unwordsSQL)
 import qualified Language.SQL.Keyword as SQL
 import qualified Language.SQL.Keyword.ConcatString as SQLs
 
+
+-- | Internal type for group-by term
 type GroupByTerm = String
+
+-- | Group-by terms type
 type GroupBys = DList GroupByTerm
 
+-- | Context state of aggregated query.
 data AggregatingContext =
   AggregatingContext
   { groupByTerms :: GroupBys
   , restriction  :: Maybe (Expr Bool)
   }
 
+-- | Initial value of 'AggregatingContext'.
 primeAggregatingContext :: AggregatingContext
 primeAggregatingContext =  AggregatingContext DList.empty Nothing
 
+-- | Add group by term into 'AggregatingContext'.
 addGroupBy :: String -> AggregatingContext -> AggregatingContext
 addGroupBy t c =  c { groupByTerms = groupByTerms c <> pure t }
 
+-- | Add having restriction into 'AggregatingContext'.
 addRestriction :: Expr (Maybe Bool) -> AggregatingContext -> AggregatingContext
 addRestriction e1 ctx =
   ctx { restriction = Just . uf . restriction $ ctx }
   where uf  Nothing  = fromTriBool e1
         uf (Just e0) = e0 `exprAnd` fromTriBool e1
 
+-- | Extract 'AggregatingContext' into SQL string.
 composeGroupBys :: AggregatingContext -> String
 composeGroupBys ac = unwordsSQL $ groupBys ++ havings
   where groupBys
