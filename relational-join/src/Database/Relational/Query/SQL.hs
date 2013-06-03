54a1f3ef2691733c687ea0050e92bc610b04c9ce
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-03 19:08:31 +0900

    Add document of simple SQL generation module.

--- relational-join/src/Database/Relational/Query/SQL.hs
+++ relational-join/src/Database/Relational/Query/SQL.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- |
+-- Module      : Database.Relational.Query.SQL
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines functions to generate simple SQL strings.
 module Database.Relational.Query.SQL (
+  -- * Update SQL
   singleKeyUpdateSQL', singleKeyUpdateSQL,
 
+  -- * Insert SQL
   insertSQL', insertSQL
   ) where
 
@@ -11,18 +23,28 @@ import qualified Language.SQL.Keyword as SQL
 import Database.Relational.Query.Table (Table, name, columns)
 
 
-singleKeyUpdateSQL' :: String -> [String] -> String -> String
+-- | Generate update SQL specified by single key.
+singleKeyUpdateSQL' :: String   -- ^ Table name
+                    -> [String] -- ^ Column name list
+                    -> String   -- ^ Key column name
+                    -> String   -- ^ Result SQL
 singleKeyUpdateSQL' table cols key =
   SQL.unwordsSQL
   $ [UPDATE, SQL.word table, SET, assignments `SQL.sepBy` ", ",
      WHERE, SQL.word key, "= ?"]
   where assignments = map (\f -> SQL.word f .=. "?") . filter (/= key) $ cols
 
-singleKeyUpdateSQL :: Table r -> String -> String
+-- | Generate update SQL specified by single key.
+singleKeyUpdateSQL :: Table r -- ^ Table metadata
+                   -> String  -- ^ Key column name
+                   -> String  -- ^ Result SQL
 singleKeyUpdateSQL tbl = singleKeyUpdateSQL' (name tbl) (columns tbl)
 
 
-insertSQL' :: String -> [String] -> String
+-- | Generate insert SQL.
+insertSQL' :: String   -- ^ Table name
+           -> [String] -- ^ Column name list
+           -> String   -- ^ Result SQL
 insertSQL' table cols =
   SQL.unwordsSQL
   $ [INSERT, INTO, SQL.word table, cols' `SQL.parenSepBy` ", ",
@@ -30,5 +52,7 @@ insertSQL' table cols =
   where cols' = map SQL.word cols
         pfs      = replicate (length cols) "?"
 
-insertSQL :: Table r -> String
+-- | Generate insert SQL.
+insertSQL :: Table r -- ^ Table metadata
+          -> String  -- ^ Result SQL
 insertSQL tbl = insertSQL' (name tbl) (columns tbl)
