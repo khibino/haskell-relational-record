d3184578a37f306919bf77ea099b5ed1c6dbb550
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-03 16:29:32 +0900

    Add document for constraint key proof object.

--- relational-join/src/Database/Relational/Query/Constraint.hs
+++ relational-join/src/Database/Relational/Query/Constraint.hs
@@ -3,7 +3,20 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+-- |
+-- Module      : Database.Relational.Query.Constraint
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module provides proof object definitions of constraint key.
+-- Difference between this module and "Database.Record.KeyConstraint" is
+-- typed constraint key column definition is included in this module.
 module Database.Relational.Query.Constraint (
+  -- * Constraint Key proof object
   Key, index, defineConstraintKey, recordConstraint, projectionKey,
 
   returnKey, appendConstraint,
@@ -23,14 +36,23 @@ import Database.Record.KeyConstraint
 import qualified Database.Record.KeyConstraint as C
 
 
-newtype Key c r ct = Key { index :: Int }
+-- | Constraint Key proof object. Constraint type 'c', record type 'r' and column type 'ft'.
+newtype Key c r ct = Key Int
 
-defineConstraintKey :: Int -> Key c r ct
+-- | Index of key which specifies constraint key.
+index :: Key c r ct -> Int
+index (Key i) = i
+
+-- | Unsafely generate constraint 'Key' proof object using specified key index.
+defineConstraintKey :: Int        -- ^ Key index which specify this constraint key
+                    -> Key c r ct -- ^ Result constraint key proof object
 defineConstraintKey =  Key
 
+-- | Get table constraint 'KeyConstraint' proof object from constraint 'Key'.
 recordConstraint :: Key c r ct -> KeyConstraint c r
 recordConstraint =  unsafeSpecifyKeyConstraint . index
 
+-- | Get projection path proof object from constraint 'Key'.
 projectionKey :: Key c r ct -> Pi r ct
 projectionKey =  defineColumn . index
 
@@ -43,25 +65,31 @@ appendConstraint :: Pi r ct -> Key c r ct
 appendConstraint =  defineConstraintKey . leafIndex
 
 
+-- | Map from table constraint into constraint 'Key'.
 mapConstraint :: (KeyConstraint c0 r -> KeyConstraint c1 r)
               -> Key c0 r ct
               -> Key c1 r ct
 mapConstraint f = returnKey . f . recordConstraint
 
+-- | Derive 'Unique' constraint 'Key' from 'Primary' constraint 'Key'
 uniqueKey :: Key Primary r ct -> Key Unique r ct
 uniqueKey  = mapConstraint C.unique
 
+-- | Derive 'NotNull' constraint 'Key' from 'Primary' constraint 'Key'
 notNullKey :: Key Primary r ct -> Key NotNull r ct
 notNullKey =  mapConstraint C.notNull
 
 
+-- | Constraint 'Key' inference interface.
 class HasConstraintKey c r ct  where
   constraintKey :: Key c r ct
 
+-- | Inference rule of 'Unique' constraint 'Key'.
 instance HasConstraintKey Primary r ct
          => HasConstraintKey Unique r ct  where
   constraintKey = uniqueKey constraintKey
 
+-- | Inference rule of 'NotNull' constraint 'Key'.
 instance HasConstraintKey Primary r ct
          => HasConstraintKey NotNull r ct  where
   constraintKey = notNullKey constraintKey
