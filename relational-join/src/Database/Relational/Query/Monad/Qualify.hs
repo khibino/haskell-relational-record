c10d95ecfa81871b75c598aaa14ef80a9c24df7c
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-05 18:20:02 +0900

    Update document of Qualify monad.

--- relational-join/src/Database/Relational/Query/Monad/Qualify.hs
+++ relational-join/src/Database/Relational/Query/Monad/Qualify.hs
@@ -1,7 +1,20 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-
+-- |
+-- Module      : Database.Relational.Query.Monad.Qualify
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines monad structure to qualify uniquely SQL table forms.
 module Database.Relational.Query.Monad.Qualify (
-  Qualified, Qualify, evalQualifyPrime, newAlias, qualifyQuery
+  -- * Qualifier type
+  Qualified,
+
+  -- * Qualify monad
+  Qualify,
+  evalQualifyPrime, newAlias, qualifyQuery
   ) where
 
 import Control.Monad (liftM, ap)
@@ -13,46 +26,60 @@ import Database.Relational.Query.Internal.AliasId (primAlias, AliasId, newAliasI
 import qualified Database.Relational.Query.Internal.AliasId as AliasId
 
 
+-- | Type for 'Qualify' monad state.
 newtype AliasIdContext = AliasIdContext { currentAliasId :: AliasId }
 
+-- | Initial state.
 primeAliasIdContext :: AliasIdContext
 primeAliasIdContext =  AliasIdContext primAlias
 
+-- | Update state function.
 nextAlias :: AliasIdContext -> (AliasId, AliasIdContext)
 nextAlias s = (cur, s { currentAliasId =  newAliasId cur })  where
   cur = currentAliasId s
 
 
+-- | Monad type to qualify SQL table forms.
 newtype Qualify a =
   Qualify { runQualify' :: State AliasIdContext a }
 
+-- | Run qualify monad.
 runQualify :: Qualify a -> AliasIdContext -> (a, AliasIdContext)
 runQualify =  runState . runQualify'
 
+-- | Run qualify monad with initial state.
 runQualifyPrime :: Qualify a -> (a, AliasIdContext)
 runQualifyPrime q = runQualify q primeAliasIdContext
 
+-- | Run qualify monad with initial state to get only result.
 evalQualifyPrime :: Qualify a -> a
 evalQualifyPrime =  fst . runQualifyPrime
 
+-- | Make qualify monad from update state function.
 qualifyState :: (AliasIdContext -> (a, AliasIdContext)) -> Qualify a
 qualifyState =  Qualify . state
 
+-- | 'Monad' instance to qualify uniquely SQL table form queries.
 instance Monad Qualify where
   return      = Qualify . return
   q0 >>= f    = Qualify $ runQualify' q0 >>= runQualify' . f
 
+-- | Define 'Functor' instance using 'Monad' methods
 instance Functor Qualify where
   fmap = liftM
 
+-- | Define 'Applicative' instance using 'Monad' methods
 instance Applicative Qualify where
   pure  = return
   (<*>) = ap
 
+-- | Generated new qualifier on internal state.
 newAlias :: Qualify AliasId
 newAlias =  qualifyState nextAlias
 
-qualifyQuery :: query -> Qualify (Qualified query)
+-- | Get qualifyed table form query.
+qualifyQuery :: query                     -- ^ Query to qualify
+             -> Qualify (Qualified query) -- ^ Result with updated state
 qualifyQuery query =
   do n <- newAlias
      return $ AliasId.qualify query n
