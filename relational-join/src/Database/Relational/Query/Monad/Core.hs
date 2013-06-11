a4bfeffd3c5fcd03b21e8356748317ec235ff91b
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-11 20:42:46 +0900

    Add document of QueryCore monad.

--- relational-join/src/Database/Relational/Query/Monad/Core.hs
+++ relational-join/src/Database/Relational/Query/Monad/Core.hs
@@ -1,9 +1,21 @@
+-- |
+-- Module      : Database.Relational.Query.Monad.Core
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines core query type.
 module Database.Relational.Query.Monad.Core (
-  QueryCore,
+  -- * Core query monad
+  QueryCore
   ) where
 
 import Database.Relational.Query.Monad.Qualify (Qualify)
 import Database.Relational.Query.Monad.Trans.Join (QueryJoin)
 
 
+-- | Core query monad type used by simple query and aggregated query.
 type QueryCore = QueryJoin Qualify
