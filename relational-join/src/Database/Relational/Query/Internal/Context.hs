c532e7389fb7e7ca6db6dd4e452527bff75bacda
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-11 17:45:19 +0900

    Add document of Join and Ordering monad transformer context.

--- relational-join/src/Database/Relational/Query/Internal/Context.hs
+++ relational-join/src/Database/Relational/Query/Internal/Context.hs
@@ -1,6 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- |
+-- Module      : Database.Relational.Query.Internal.Context
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module provides context definition for
+-- "Database.Relational.Query.Monad.Trans.Join" and
+-- "Database.Relational.Query.Monad.Trans.Ordering".
 module Database.Relational.Query.Internal.Context (
+  -- * Join context
   Context,
 
   primeContext,
@@ -10,12 +23,13 @@ module Database.Relational.Query.Internal.Context (
 
   composeSQL,
 
+  -- * Ordering context
   Order (..), OrderBys,
   OrderingContext,
 
   primeOrderingContext,
 
-  updateOrderBy, takeOrderBys, restoreLowOrderBys,
+  updateOrderBy, -- takeOrderBys, restoreLowOrderBys,
 
   composeOrderBys
   ) where
@@ -39,19 +53,21 @@ import Language.SQL.Keyword (Keyword(..), unwordsSQL)
 import qualified Language.SQL.Keyword as SQL
 
 
--- Base contexts
-
+-- | Context type for QueryJoin.
 data Context = Context
                { product :: Maybe QueryProductNode
                , restriction :: Maybe (Expr Bool)
                }
 
+-- | Initial 'Context'.
 primeContext :: Context
 primeContext =  Context Nothing Nothing
 
+-- | Update product of 'Context'.
 updateProduct' :: (Maybe QueryProductNode -> Maybe QueryProductNode) -> Context -> Context
 updateProduct' uf ctx = ctx { product = uf . product $ ctx }
 
+-- | Update product of 'Context'.
 updateProduct :: (Maybe QueryProductNode -> QueryProductNode) -> Context -> Context
 updateProduct uf = updateProduct' (Just . uf)
 
@@ -61,12 +77,14 @@ updateProduct uf = updateProduct' (Just . uf)
 -- restoreLeft :: QueryProductNode -> Product.NodeAttr -> Context -> Context
 -- restoreLeft pL naR ctx = updateProduct (Product.growLeft pL naR) ctx
 
+-- | Add restriction of 'Context'.
 addRestriction :: Expr (Maybe Bool) -> Context -> Context
 addRestriction e1 ctx =
   ctx { restriction = Just . uf . restriction $ ctx }
   where uf  Nothing  = fromTriBool e1
         uf (Just e0) = e0 `exprAnd` fromTriBool e1
 
+-- | Compose SQL String from QueryJoin monad object.
 composeSQL' :: Projection r -> QueryProduct -> Maybe (Expr Bool) -> String
 composeSQL' pj pd re =
   unwordsSQL
@@ -79,37 +97,45 @@ composeSQL' pj pd re =
                     [(0 :: Int)..]
           wheres  = Prelude.maybe [] (\e -> [WHERE, SQL.word . showExpr $ e])
 
+-- | Compose SQL String from QueryJoin monad object.
 composeSQL :: Projection r -> Context -> String
 composeSQL pj c = composeSQL' pj
                   (maybe (error "relation: empty product!") (Product.nodeTree) (product c))
                   (restriction c)
 
 
--- Ordering contexts
-
+-- | Order attribute.
 data Order = Asc | Desc
 
+-- | Order-by terms.
 type OrderBys = DList (Order, String)
 
+-- | Context type for Orderings.
 newtype OrderingContext = OrderingContext { orderBys :: OrderBys }
 
+-- | Initial 'OrderingContext'
 primeOrderingContext :: OrderingContext
 primeOrderingContext =  OrderingContext DList.empty
 
+-- | Add order-by term.
 updateOrderBy :: Order -> String -> OrderingContext -> OrderingContext
 updateOrderBy order' term ctx =
   ctx { orderBys = orderBys ctx <> pure (order', term)  }
 
+{-
 takeOrderBys :: OrderingContext -> (OrderBys, OrderingContext)
 takeOrderBys ctx = (orderBys ctx , ctx { orderBys = DList.empty })
 
 restoreLowOrderBys :: OrderBys -> OrderingContext -> OrderingContext
 restoreLowOrderBys ros ctx = ctx { orderBys = orderBys ctx <> ros }
+-}
 
+-- | Get SQL keyword from order attribute.
 order :: Order -> Keyword
 order Asc  = ASC
 order Desc = DESC
 
+-- | Concatinate order-by terms into SQL string.
 composeOrderBys :: OrderingContext -> String
 composeOrderBys oc = unwordsSQL orders  where
   orderList = DList.foldr (\ (o, e) r -> [SQL.word e, order o] `SQL.sepBy` " "  : r) []
