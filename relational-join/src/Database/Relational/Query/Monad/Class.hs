f06060cbb3c4e8e8a509d2602dd53eea873d5c0e
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-11 14:05:02 +0900

    Add document of query building interfaces.

--- relational-join/src/Database/Relational/Query/Monad/Class.hs
+++ relational-join/src/Database/Relational/Query/Monad/Class.hs
@@ -1,6 +1,17 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
+-- |
+-- Module      : Database.Relational.Query.Monad.Class
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines query building interface classes.
 module Database.Relational.Query.Monad.Class (
+  -- * Query interface classes
   MonadQualify (..),
   MonadQuery (..), MonadAggregate (..)
   ) where
@@ -12,15 +23,31 @@ import Database.Relational.Query.Sub (SubQuery, Qualified)
 
 import Database.Relational.Query.Internal.Product (NodeAttr)
 
+-- | Query building interface.
 class (Functor m, Monad m) => MonadQuery m where
-  on     :: Expr (Maybe Bool) -> m ()
-  wheres :: Expr (Maybe Bool) -> m ()
-  unsafeSubQuery :: NodeAttr -> Qualified SubQuery -> m (Projection r)
+  -- | Add restriction to last join.
+  on :: Expr (Maybe Bool) -- ^ 'Expr' which represent restriction
+     -> m ()              -- ^ Restricted query context
+  -- | Add restriction to this query.
+  wheres :: Expr (Maybe Bool) -- ^ 'Expr' which represent restriction
+         -> m ()              -- ^ Restricted query context
+  -- | Unsafely join subquery with this query.
+  unsafeSubQuery :: NodeAttr           -- ^ Attribute maybe or just
+                 -> Qualified SubQuery -- ^ 'SubQuery' to join
+                 -> m (Projection r)   -- ^ Result joined context and 'SubQuery' result projection.
   -- unsafeMergeAnotherQuery :: NodeAttr -> m (Projection r) -> m (Projection r)
 
+-- | Lift interface from base qualify monad.
 class (Functor q, Monad q, MonadQuery m) => MonadQualify q m where
+  -- | Lift from qualify monad 'q' into 'MonadQuery' m.
+  --   Qualify monad qualifies table form 'SubQuery'.
   liftQualify :: q a -> m a
 
+-- | Aggregated query building interface extends 'MonadQuery'.
 class MonadQuery m => MonadAggregate m where
-  groupBy :: Projection r -> m (Aggregation r)
-  having  :: Aggregation (Maybe Bool) -> m ()
+  -- | Add /group by/ term into context and get aggregated projection.
+  groupBy :: Projection r      -- ^ Projection to add into group by
+          -> m (Aggregation r) -- ^ Result context and aggregated projection
+  -- | Add restriction to this aggregated query.
+  having :: Aggregation (Maybe Bool) -- ^ 'Aggregation' which represent restriction
+         -> m ()                     -- ^ Restricted query context
