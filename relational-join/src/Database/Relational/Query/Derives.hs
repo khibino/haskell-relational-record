5f9ed8a3876c3243fc173bfaf672791766385882
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-12 16:39:08 +0900

    Add document of typed SQLs derived from type informations.

--- relational-join/src/Database/Relational/Query/Derives.hs
+++ relational-join/src/Database/Relational/Query/Derives.hs
@@ -1,5 +1,15 @@
 {-# LANGUAGE FlexibleContexts #-}
 
+-- |
+-- Module      : Database.Relational.Query.Type
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines typed SQLs derived from type informations.
 module Database.Relational.Query.Derives (
   unique,
 
@@ -22,31 +32,41 @@ import Database.Relational.Query.Constraint
 import qualified Database.Relational.Query.Constraint as Constraint
 import Database.Relational.Query.Type (Update, typedSingleKeyUpdate)
 
+
+-- | Query restricted with specified unique key.
 unique :: PersistableWidth p
-       => Key Unique a p
-       -> Relation () a
-       -> Relation p a
+       => Key Unique a p -- ^ Unique key proof object which record type is 'a' and key type is 'p'.
+       -> Relation () a  -- ^ 'Relation' to add restriction.
+       -> Relation p a   -- ^ Result restricted 'Relation'
 unique uk rel = relation' $ do
   q <- query rel
   (param, ()) <- placeholder (\ph -> wheres $ q ! projectionKey uk .=. ph)
   return (param, q)
 
+-- | Query restricted with specified primary key.
 primary' :: PersistableWidth p
-         => Key Primary a p
-         -> Relation () a
-         -> Relation p a
+         => Key Primary a p -- ^ Primary key proof object which record type is 'a' and key type is 'p'.
+         -> Relation () a   -- ^ 'Relation' to add restriction.
+         -> Relation p a    -- ^ Result restricted 'Relation'
 primary' pc = unique $ Constraint.uniqueKey pc
 
+-- | Query restricted with infered primary key.
 primary :: (PersistableWidth p, HasConstraintKey Primary a p)
-        => Relation () a
-        -> Relation p a
+        => Relation () a -- ^ 'Relation' to add restriction.
+        -> Relation p a  -- ^ Result restricted 'Relation'
 primary = primary' constraintKey
 
 
-updateByConstraintKey :: PersistableWidth p => Table r -> Key c r p -> Update p r
+-- | Typed 'Update' using specified key.
+updateByConstraintKey :: PersistableWidth p
+                      => Table r    -- ^ 'Table' to update
+                      -> Key c r p  -- ^ Key with constraint 'c', record type 'r' and column type 'p'
+                      -> Update p r -- ^ Result typed 'Update'
 updateByConstraintKey table key =
   typedSingleKeyUpdate table (table `Table.index` Constraint.index key)
 
+-- | Typed 'Update' using infered primary key.
 primaryUpdate :: (PersistableWidth p, HasConstraintKey Primary r p)
-              => Table r -> Update p r
+              => Table r   -- ^ 'Table' to update
+                 -> Update p r -- ^ Result typed 'Update'
 primaryUpdate table = updateByConstraintKey table (uniqueKey constraintKey)
