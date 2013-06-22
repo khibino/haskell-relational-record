ef9641ee02d75666c7519f497d2e17d9e638c5ec
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-22 14:33:18 +0900

    Add document of schema driver interface via HDBC.

--- relational-query-HDBC/src/Database/HDBC/Schema/Driver.hs
+++ relational-query-HDBC/src/Database/HDBC/Schema/Driver.hs
@@ -6,6 +6,9 @@
 -- Maintainer  : ex8k.hibino@gmail.com
 -- Stability   : experimental
 -- Portability : unknown
+--
+-- This module provides driver interface
+-- to load database system catalog via HDBC.
 module Database.HDBC.Schema.Driver (
   TypeMap,
   Driver(Driver, typeMap, getFieldsWithMap, getPrimaryKey),
@@ -16,24 +19,35 @@ module Database.HDBC.Schema.Driver (
 import Database.HDBC (IConnection)
 import Language.Haskell.TH (TypeQ)
 
+
+-- | Mapping between type name string of DBMS and type in Haskell.
+--   Type name string depends on specification of DBMS system catalogs.
 type TypeMap = [(String, TypeQ)]
 
+-- | Interface type to load database system catalog via HDBC.
 data Driver conn =
   Driver
-  { typeMap   :: TypeMap
-  , getFieldsWithMap :: TypeMap
-                        -> conn
-                        -> String
-                        -> String
-                        -> IO ([(String, TypeQ)], [Int])
-  , getPrimaryKey :: conn
-                  -> String
-                  -> String
-                  -> IO (Maybe String)
+  { -- | Custom type mapping of this driver
+    typeMap   :: TypeMap
+
+    -- | Get column name and Haskell type pairs and not-null columns index.
+  , getFieldsWithMap :: TypeMap                       --  Custom type mapping
+                     -> conn                          --  Connection to query system catalog
+                     -> String                        --  Schema name string
+                     -> String                        --  Table name string
+                     -> IO ([(String, TypeQ)], [Int]) --  Action to get column name and Haskell type pairs and not-null columns index.
+
+    -- | Get primary key column name.
+  , getPrimaryKey :: conn              --  Connection to query system catalog
+                  -> String            --  Schema name string
+                  -> String            --  Table name string
+                  -> IO (Maybe String) --  Action to get primary key column name
   }
 
+-- | Empty definition of 'Driver'
 emptyDriver :: IConnection conn => Driver conn
 emptyDriver =  Driver [] (\_ _ _ _ -> return ([],[])) (\_ _ _ -> return Nothing)
 
+-- | Helper function to call 'getFieldsWithMap' using 'typeMap' of 'Driver'.
 getFields :: IConnection conn => Driver conn -> conn -> String -> String -> IO ([(String, TypeQ)], [Int])
 getFields drv = getFieldsWithMap drv (typeMap drv)
