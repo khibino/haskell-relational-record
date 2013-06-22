e2fffab5bb0ae87653a0660d905aafc00cd07a28
Kei Hibino <ex8k.hibino@gmail.com>
2013-06-22 17:58:27 +0900

    Remove unused definitions from example.

--- relational-join-HDBC-pgTest/pgTest/PgTest.hs
+++ relational-join-HDBC-pgTest/pgTest/PgTest.hs
@@ -1,26 +1,6 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-
 module PgTest  where
 
 import Distribution.TestSuite (Test)
-import PgTestDataSource (defineTable)
--- import Database.HDBC.PostgreSQL (connectPostgreSQL, Connection)
--- import Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
--- import Database.HDBC.Schema.Driver (typeMap)
--- import Database.HDBC.TH (defineTableFromDB)
-import Data.ByteString (ByteString)
-import Data.Text (Text)
 
 tests :: [Test]
 tests =  []
-
-$(defineTable
-  [("varchar", [t| ByteString |]),
-   ("text", [t| Text |])
-  ]
-  "TEST" "test_table0" [])
--- defineTableFromDB
---   (connectPostgreSQL "dbname=testdb")
---   (driverPostgreSQL { typeMap = [("varchar", [t| Text |])] })
---   "TEST" "test_table0" []
