47b29164d65a23fb61ec2b535c75bda3c0eaafa8
Kazu Yamamoto <kazu@iij.ad.jp>
2013-07-02 15:46:43 +0900

    using "::date".

--- doc/tutorial/add.sql
+++ doc/tutorial/add.sql
@@ -451,14 +451,14 @@ where fed_id = '04-4444444';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Woburn' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2000-01-15' open_date, '2005-01-04' last_date,
+ (select 'CHK' prod_cd, '2000-01-15'::date open_date, '2005-01-04'::date last_date,
     1057.75 avail, 1057.75 pend union all
   select 'SAV' prod_cd, '2000-01-15' open_date, '2004-12-19' last_date,
     500.00 avail, 500.00 pend union all
@@ -468,14 +468,14 @@ where c.fed_id = '111-11-1111';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Woburn' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2001-03-12' open_date, '2004-12-27' last_date,
+ (select 'CHK' prod_cd, '2001-03-12'::date open_date, '2004-12-27'::date last_date,
     2258.02 avail, 2258.02 pend union all
   select 'SAV' prod_cd, '2001-03-12' open_date, '2004-12-11' last_date,
     200.00 avail, 200.00 pend) a
@@ -483,14 +483,14 @@ where c.fed_id = '222-22-2222';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Quincy' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2002-11-23' open_date, '2004-11-30' last_date,
+ (select 'CHK' prod_cd, '2002-11-23'::date open_date, '2004-11-30'::date last_date,
     1057.75 avail, 1057.75 pend union all
   select 'MM' prod_cd, '2002-12-15' open_date, '2004-12-05' last_date,
     2212.50 avail, 2212.50 pend) a
@@ -498,14 +498,14 @@ where c.fed_id = '333-33-3333';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Waltham' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2003-09-12' open_date, '2005-01-03' last_date,
+ (select 'CHK' prod_cd, '2003-09-12'::date open_date, '2005-01-03'::date last_date,
     534.12 avail, 534.12 pend union all
   select 'SAV' prod_cd, '2000-01-15' open_date, '2004-10-24' last_date,
     767.77 avail, 767.77 pend union all
@@ -515,27 +515,27 @@ where c.fed_id = '444-44-4444';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Salem' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2004-01-27'::varchar open_date, '2005-01-05'::varchar last_date,
+ (select 'CHK' prod_cd, '2004-01-27'::date open_date, '2005-01-05'::date last_date,
     2237.97 avail, 2897.97 pend) a
 where c.fed_id = '555-55-5555';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Waltham' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2002-08-24' open_date, '2004-11-29' last_date,
+ (select 'CHK' prod_cd, '2002-08-24'::date open_date, '2004-11-29'::date last_date,
     122.37 avail, 122.37 pend union all
   select 'CD' prod_cd, '2004-12-28' open_date, '2004-12-28' last_date,
     10000.00 avail, 10000.00 pend) a
@@ -543,27 +543,27 @@ where c.fed_id = '666-66-6666';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Woburn' limit 1) e
   cross join
- (select 'CD' prod_cd, '2004-01-12'::varchar open_date, '2004-01-12'::varchar last_date,
+ (select 'CD' prod_cd, '2004-01-12'::date open_date, '2004-01-12'::date last_date,
     5000.00 avail, 5000.00 pend) a
 where c.fed_id = '777-77-7777';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Salem' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2001-05-23' open_date, '2005-01-03' last_date,
+ (select 'CHK' prod_cd, '2001-05-23'::date open_date, '2005-01-03'::date last_date,
     3487.19 avail, 3487.19 pend union all
   select 'SAV' prod_cd, '2001-05-23' open_date, '2004-10-12' last_date,
     387.99 avail, 387.99 pend) a
@@ -571,14 +571,14 @@ where c.fed_id = '888-88-8888';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Waltham' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2003-07-30' open_date, '2004-12-15' last_date,
+ (select 'CHK' prod_cd, '2003-07-30'::date open_date, '2004-12-15'::date last_date,
     125.67 avail, 125.67 pend union all
   select 'MM' prod_cd, '2004-10-28' open_date, '2004-10-28' last_date,
     9345.55 avail, 9845.55 pend union all
@@ -590,14 +590,14 @@ where c.fed_id = '999-99-9999';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Salem' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2002-09-30' open_date, '2004-12-15' last_date,
+ (select 'CHK' prod_cd, '2002-09-30'::date open_date, '2004-12-15'::date last_date,
     23575.12 avail, 23575.12 pend union all
   select 'BUS' prod_cd, '2002-10-01' open_date, '2004-08-28' last_date,
     0 avail, 0 pend) a
@@ -605,40 +605,40 @@ where c.fed_id = '04-1111111';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Woburn' limit 1) e
   cross join
- (select 'BUS' prod_cd, '2004-03-22'::varchar open_date, '2004-11-14'::varchar last_date,
+ (select 'BUS' prod_cd, '2004-03-22'::date open_date, '2004-11-14'::date last_date,
     9345.55 avail, 9345.55 pend) a
 where c.fed_id = '04-2222222';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Salem' limit 1) e
   cross join
- (select 'CHK' prod_cd, '2003-07-30'::varchar open_date, '2004-12-15'::varchar last_date,
+ (select 'CHK' prod_cd, '2003-07-30'::date open_date, '2004-12-15'::date last_date,
     38552.05 avail, 38552.05 pend) a
 where c.fed_id = '04-3333333';
 insert into account (product_cd, cust_id, open_date,
   last_activity_date, status, open_branch_id,
   open_emp_id, avail_balance, pending_balance)
-select a.prod_cd, c.cust_id, to_date(a.open_date,'YYYY-MM-DD'), to_date(a.last_date,'YYYY-MM-DD'), 'ACTIVE',
+select a.prod_cd, c.cust_id, a.open_date, a.last_date, 'ACTIVE',
   e.branch_id, e.emp_id, a.avail, a.pend
 from customer c cross join 
  (select b.branch_id, e.emp_id 
   from branch b inner join employee e on e.assigned_branch_id = b.branch_id
   where b.city = 'Quincy' limit 1) e
   cross join
- (select 'SBL' prod_cd, '2004-02-22'::varchar open_date, '2004-12-17'::varchar last_date,
+ (select 'SBL' prod_cd, '2004-02-22'::date open_date, '2004-12-17'::date last_date,
     50000.00 avail, 50000.00 pend) a
 where c.fed_id = '04-4444444';
 
