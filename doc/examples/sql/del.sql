4f70383e8dd053ab6e25c22bd95f47414209358f
Kazu Yamamoto <kazu@iij.ad.jp>
2013-07-03 13:59:45 +0900

    using schema.

---
+++ doc/examples/sql/del.sql
@@ -0,0 +1,24 @@
+/*
+   % psql -f del.sql learningsql
+   % dropdb learningsql
+ */
+
+drop table LEARNINGSQL.transaction;
+drop table LEARNINGSQL.account;
+drop table LEARNINGSQL.officer;
+drop table LEARNINGSQL.business;
+drop table LEARNINGSQL.individual;
+drop table LEARNINGSQL.customer;
+drop table LEARNINGSQL.product;
+drop table LEARNINGSQL.product_type;
+drop table LEARNINGSQL.employee;
+drop table LEARNINGSQL.branch;
+drop table LEARNINGSQL.department;
+
+drop type cust_type_cd_t;
+drop type status_t;
+drop type txn_type_cd_t;
+
+drop schema LEARNINGSQL;
+
+
