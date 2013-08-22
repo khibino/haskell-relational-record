38b4b2590bbf16a1f5ad2e046d4f324709d61bac
Kei Hibino <ex8k.hibino@gmail.com>
2013-08-22 17:38:06 +0900

    Add restricted context type using table projection.

--- relational-join/src/Database/Relational/Query/Monad/Restrict.hs
+++ relational-join/src/Database/Relational/Query/Monad/Restrict.hs
@@ -10,7 +10,7 @@
 -- This module contains definitions about simple restrict context monad type.
 module Database.Relational.Query.Monad.Restrict (
   -- * Monad to restrict target records.
-  Restrict,
+  Restrict, RestrictedStatement,
 
   -- restricted,
   expandWhere
@@ -18,12 +18,18 @@ module Database.Relational.Query.Monad.Restrict (
 
 import Data.Functor.Identity (Identity (..), runIdentity)
 
+import Database.Relational.Query.Projection (Projection)
 import Database.Relational.Query.Monad.Trans.Restricting
   (Restrictings, WherePrepend, extractWheres)
 
 
 -- | Restrict only monad type used from update statement and delete statement.
 type Restrict = Restrictings Identity
+
+-- | RestrictedStatement type synonym.
+--   Projection record type 'r' must be
+--   the same as 'Restrictings' type parameter 'r'.
+type RestrictedStatement r a = Projection r -> Restrict a
 
 -- -- | 'return' of 'Restrict'
 -- restricted :: a -> Restrict a
