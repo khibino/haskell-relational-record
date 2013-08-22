1fdd33ed610fdf71b0f0b1aafba8873dbd63d073
Kei Hibino <ex8k.hibino@gmail.com>
2013-08-22 18:54:25 +0900

    Update example.

--- relational-join-HDBC-pgTest/example/4/create.sh
+++ relational-join-HDBC-pgTest/example/4/create.sh
@@ -23,11 +23,4 @@ set -x
 psql -c "CREATE SCHEMA EXAMPLE4" testdb
 psql -c "$create_setA_table" testdb
 
-insert() {
-	psql -c "INSERT INTO EXAMPLE4.set_$1 (seq, name) VALUES ($2, '$3')" testdb
-}
-
-insert a 1 'Apple'
-insert a 2 'Orange'
-insert a 5 'Banana'
-insert a 6 'Cherry'
+runghc -i.. insert.hs
