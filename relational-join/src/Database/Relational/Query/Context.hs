3dc3098378ec8ee66d8573237917487927bffb8f
Kei Hibino <ex8k.hibino@gmail.com>
2013-08-25 11:50:08 +0900

    Query context tag type begins.

---
+++ relational-join/src/Database/Relational/Query/Context.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE EmptyDataDecls #-}
+
+-- |
+-- Module      : Database.Relational.Query.Context
+-- Copyright   : 2013 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines query context tag types.
+module Database.Relational.Query.Context (
+  Flat, Aggregated
+  ) where
+
+-- | Type tag for flat (not-aggregated) query
+data Flat
+
+-- | Type tag for aggregated query
+data Aggregated
