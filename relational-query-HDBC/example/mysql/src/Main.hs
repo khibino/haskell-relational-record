5e392d8c7c40ed45e9330018d5f7aa1084970661
krdlab <krdlab@gmail.com>
2014-02-21 18:19:11 +0900

    Change sample queries in the example

--- relational-query-HDBC/example/mysql/src/Main.hs
+++ relational-query-HDBC/example/mysql/src/Main.hs
@@ -28,18 +28,22 @@ main = handleSqlError' $ withConnectionIO connect $ \conn -> do
     where
         printResults c q = runQuery c (relationalQuery q) () >>= print
 
+        completed = relation
+            [ u
+            | u  <- query user
+            , () <- wheres $ u ! U.completed' .=. value 1
+            ]
+
         sample1 :: Relation () (String, Day)
         sample1 = relation
             [ u ! U.name' >< u ! U.createdAt'
-            | u  <- query user
-            , () <- wheres $ u ! U.completed' .=. value 1
+            | u  <- query completed
             ]
 
         sample2 :: Relation () (String, Day)
         sample2 = relation
             [ u ! U.email' >< u ! U.createdAt'
-            | u  <- query user
-            , () <- wheres $ u ! U.completed' .=.  value 1
+            | u  <- query completed
             , () <- wheres $ u ! U.createdAt' .>=. value (day 2014 2 10)
             ]
 
