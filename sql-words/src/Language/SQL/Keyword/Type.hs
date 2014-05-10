51cab2f134c4cdf9b85e797a8866adae53167ac3
Kei Hibino <ex8k.hibino@gmail.com>
2014-05-10 17:44:41 +0900

    Update SQL Keyword implementation using internal diff-string type.

--- sql-words/src/Language/SQL/Keyword/Type.hs
+++ sql-words/src/Language/SQL/Keyword/Type.hs
@@ -23,6 +23,20 @@ import Data.String (IsString(fromString))
 import Data.List (find)
 
 
+newtype DString = DString (String -> String)
+
+dString :: String -> DString
+dString =  DString . (++)
+
+showDString :: DString -> String
+showDString (DString f) = f []
+
+instance Show DString where
+  show = showDString
+
+instance Read DString where
+  readsPrec _ s = [(dString s, [])]
+
 -- | Type represent SQL keywords.
 data Keyword = SELECT | ALL | DISTINCT | ON
              | GROUP | COUNT | SUM | AVG | MAX | MIN | EVERY | ANY | SOME
@@ -63,7 +77,7 @@ data Keyword = SELECT | ALL | DISTINCT | ON
 
              | DATE | TIME | TIMESTAMP | INTERVAL
 
-             | Sequence String
+             | Sequence DString
              deriving (Read, Show)
 
              {-
@@ -73,6 +87,10 @@ data Keyword = SELECT | ALL | DISTINCT | ON
              -}
 
 
+-- | Make 'Keyword' from String
+word :: String -> Keyword
+word =  Sequence . dString
+
 -- | 'Keyword' type with OverloadedString extension,
 --   can be involved same list with string literals.
 --
@@ -80,17 +98,13 @@ data Keyword = SELECT | ALL | DISTINCT | ON
 --
 instance IsString Keyword where
   fromString s' = found (find ((== "") . snd) (reads s')) s'  where
-   found  Nothing      s = Sequence s
+   found  Nothing      s = word s
    found (Just (w, _)) _ = w
-
--- | Make 'Keyword' from String
-word :: String -> Keyword
-word =  Sequence
 
 -- | Show 'Keyword'
 wordShow :: Keyword -> String
 wordShow =  d  where
-  d (Sequence s)   = s
+  d (Sequence s)   = showDString s
   d w              = show w
 
 -- | Concatinate keywords into 'String' like unwords
@@ -99,11 +113,11 @@ unwordsSQL =  unwords . map wordShow
 
 -- | Make SQL string expression. No escape logic, so this is unsafe function.
 unsafeString :: String -> Keyword
-unsafeString =  Sequence . ('\'' :) . (++ "'")
+unsafeString =  word . ('\'' :) . (++ "'")
 
 -- | Make SQL integer expression
 integer :: (Integral a, Show a) => a -> Keyword
-integer =  Sequence . show
+integer =  word . show
 
 -- | Map 'String' function into 'Keyword' function
 stringMap :: (String -> String) -> Keyword -> Keyword
