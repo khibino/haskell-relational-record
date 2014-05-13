cf5c6e553ae03a6c3a1ed4629bfb2a7fdc785309
Kei Hibino <ex8k.hibino@gmail.com>
2014-05-13 15:20:49 +0900

    Change StringSQL implementation into SQL.Keyword type.

--- relational-query/src/Database/Relational/Query/Internal/SQL.hs
+++ relational-query/src/Database/Relational/Query/Internal/SQL.hs
@@ -9,16 +9,26 @@
 --
 -- This module provides SQL string wrap interfaces.
 module Database.Relational.Query.Internal.SQL (
-  StringSQL, stringSQL, showStringSQL
+  StringSQL, stringSQL, showStringSQL, rowStringSQL
   ) where
 
+import Language.SQL.Keyword (Keyword, word, wordShow, fold, (|*|), paren)
+
+
 -- | String wrap type for SQL strings.
-type StringSQL = ShowS
+type StringSQL = Keyword
 
 -- | 'StringSQL' from 'String'.
 stringSQL :: String -> StringSQL
-stringSQL =  (++)
+stringSQL =  word
 
 -- | 'StringSQL' to 'String'.
 showStringSQL :: StringSQL -> String
-showStringSQL =  ($ [])
+showStringSQL =  wordShow
+
+-- | Row String of SQL values.
+rowStringSQL :: [StringSQL] -> StringSQL
+rowStringSQL =  d  where
+  d []  = error "Projection: no columns."
+  d [c] = c
+  d cs  = paren $ fold (|*|) cs
