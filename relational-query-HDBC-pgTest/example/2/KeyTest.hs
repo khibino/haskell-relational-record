88fc88800972459d6e768e1254ff28b3b558f698
Kei Hibino <ex8k.hibino@gmail.com>
2014-05-24 01:17:09 +0900

    Update example.

--- relational-query-HDBC-pgTest/example/2/KeyTest.hs
+++ relational-query-HDBC-pgTest/example/2/KeyTest.hs
@@ -3,6 +3,14 @@
 module KeyTest where
 
 import PgTestDataSource (defineTable)
+import Database.HDBC.Query.TH (makeRecordPersistableDefault)
 
 $(defineTable []
   "EXAMPLE2" "keyTest" [])
+
+
+data Foo = Foo Int String
+data Bar = Bar { x :: Int, y :: String }
+
+$(makeRecordPersistableDefault ''Foo)
+$(makeRecordPersistableDefault ''Bar)
