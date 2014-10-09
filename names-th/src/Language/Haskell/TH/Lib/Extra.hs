c2f2fb7820543d7f90b707671046869135b56373
Kei Hibino <ex8k.hibino@gmail.com>
2014-10-09 21:30:34 +0900

    Functions to put informational message and warning message while compile time.

--- names-th/src/Language/Haskell/TH/Lib/Extra.hs
+++ names-th/src/Language/Haskell/TH/Lib/Extra.hs
@@ -19,17 +19,21 @@ module Language.Haskell.TH.Lib.Extra (
   pprQ,
 
   -- * Functions to raise compile error
-  -- $compileError
-  compileErrorIO, compileError,
+  -- $compileMessage
+  compileMessage, compileWarning, compileError,
 
+  compileErrorIO
   ) where
 
+import System.IO (hPutStrLn, stderr)
+import System.Environment (lookupEnv)
+
 import Language.Haskell.TH
-  (Ppr, ppr, Q, runQ,
+  (Ppr, ppr, Q, runQ, runIO,
    Name, Dec, sigD, valD, TypeQ, varP, normalB,
    ExpQ, litE, integerL)
 import Language.Haskell.TH.PprLib (Doc)
-import Language.Haskell.TH.Syntax (Quasi)
+import Language.Haskell.TH.Syntax (Quasi, qReport)
 
 {- $extraTemplateFunctions
 Extra functions to generate haskell templates.
@@ -58,10 +62,24 @@ Pretty printing for haskell templates.
 pprQ :: (Functor m, Quasi m, Ppr a) => Q a -> m Doc
 pprQ =  fmap ppr . runQ
 
-{- $compileError
+{- $compileMessage
 Functions to raise compile error from codes
 which generating haskell templates.
 -}
+
+-- | Print compile message from TH code.
+compileMessage :: Quasi m => String -> m ()
+compileMessage s = runQ . runIO $ do
+  mayOut <- lookupEnv "TH_EXTRA_MESSAGE_OUTPUT"
+  case mayOut of
+    Just out
+      | out `elem` ["as_warn", "as_warning"]  ->  compileWarning s
+      | otherwise                             ->  hPutStrLn stderr s
+    Nothing                                   ->  return ()
+
+-- | Print compile warnings from TH code.
+compileWarning :: Quasi m => String -> m ()
+compileWarning =  qReport False
 
 -- | Raise compile error from TH code.
 compileError :: Quasi m => String -> m a
