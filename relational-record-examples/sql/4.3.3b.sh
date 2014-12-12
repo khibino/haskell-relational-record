a2302c8cfdb301349195c2fda0080faee1e63df6
Shohei Murayama <shohei.murayama@gmail.com>
2014-12-12 14:45:27 +0900

    add new sql as example

---
+++ relational-record-examples/sql/4.3.3b.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sqlite3 examples.db "
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE product_cd IN (SELECT product_cd FROM product
+WHERE product_type_cd = 'ACCOUNT')
+;"
+
