f8570f696c4204e1b341bdbee79334dc4f1ae688
Kei Hibino <ex8k.hibino@gmail.com>
2015-01-03 23:38:10 +0900

    Fix CI.

--- travis-CI/before-install.sh
+++ travis-CI/before-install.sh
@@ -3,5 +3,7 @@
 set -x
 
 cat /proc/cpuinfo
+
 sudo apt-get update
+sudo apt-get install cabal-install
 cabal update
--- travis-CI/install.sh
+++ travis-CI/install.sh
@@ -3,7 +3,6 @@
 set -x
 
 sudo apt-get install haskell-platform haskell-platform-doc haskell-platform-prof
-sudo apt-get install cabal-install
 sudo apt-get install libghc-dlist-dev libghc-dlist-prof libghc-dlist-doc
 sudo apt-get install libghc-convertible-dev libghc-convertible-prof libghc-convertible-doc
 sudo apt-get install libghc-hdbc-dev libghc-hdbc-prof libghc-hdbc-doc
