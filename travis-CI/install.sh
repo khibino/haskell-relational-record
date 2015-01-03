7a68b95d289ceb959167e07ad9012ffd6ca8546c
Kei Hibino <ex8k.hibino@gmail.com>
2015-01-03 23:47:53 +0900

    Fix install sequence of CI.

--- travis-CI/install.sh
+++ travis-CI/install.sh
@@ -7,5 +7,4 @@ sudo apt-get install libghc-dlist-dev libghc-dlist-prof libghc-dlist-doc
 sudo apt-get install libghc-convertible-dev libghc-convertible-prof libghc-convertible-doc
 sudo apt-get install libghc-hdbc-dev libghc-hdbc-prof libghc-hdbc-doc
 
-cabal install time-locale-compat
-cabal install cabal-test-compat
+cabal install relational-record
