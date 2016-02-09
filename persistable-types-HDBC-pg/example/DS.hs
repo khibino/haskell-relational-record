b292f104d882fd4a579af1ee9bfe23abad7bb6e2
Kei Hibino <ex8k.hibino@gmail.com>
2016-02-09 09:42:43 +0900

    Update example along with interface updates.

--- persistable-types-HDBC-pg/example/DS.hs
+++ persistable-types-HDBC-pg/example/DS.hs
@@ -2,8 +2,7 @@
 
 module DS (definePgTable) where
 
-import Language.Haskell.TH (Q, Dec)
-import Language.Haskell.TH.Name.CamelCase (ConName)
+import Language.Haskell.TH (Q, Dec, Name)
 import Database.HDBC.PostgreSQL (connectPostgreSQL)
 import Database.HDBC.Query.TH (defineTableFromDB)
 import Database.HDBC.Schema.Driver (Driver (..))
@@ -14,6 +13,6 @@ addNetAddress :: Driver conn -> Driver conn
 addNetAddress d =
   d { typeMap = [ ("inet", [t| Inet |]), ("cidr", [t| Cidr |] ) ] ++ typeMap d }
 
-definePgTable :: String -> String -> [ConName] -> Q [Dec]
+definePgTable :: String -> String -> [Name] -> Q [Dec]
 definePgTable =
   defineTableFromDB (connectPostgreSQL "dbname=testdb") (addNetAddress driverPostgreSQL)
--- persistable-types-HDBC-pg/example/InetExample.hs
+++ persistable-types-HDBC-pg/example/InetExample.hs
@@ -4,10 +4,9 @@
 
 module SinglePKey where
 
-import Language.Haskell.TH.Name.CamelCase (ConName (..))
 import DS
 import Database.HDBC.PostgreSQL.Persistable ()
 
 $(definePgTable
   "EXAMPLE" "inet_example"
-  [ConName n | n <- [''Eq, ''Show]])
+  [''Eq, ''Show])
