0a2c2b91c702ab1df073c7498064e35c5ed80c38
Blaz Repas <info@bss.si>
2016-02-28 19:50:13 +0100

    Fix missing imports for UTCTime and ZonedTime support

--- relational-query/src/Database/Relational/Query/Pure.hs
+++ relational-query/src/Database/Relational/Query/Pure.hs
@@ -29,7 +29,7 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as LT
 import Text.Printf (PrintfArg, printf)
-import Data.Time (FormatTime, Day, TimeOfDay, LocalTime, formatTime)
+import Data.Time (FormatTime, Day, TimeOfDay, LocalTime, UTCTime, ZonedTime, formatTime)
 import Data.Time.Locale.Compat (defaultTimeLocale)
 
 import Language.SQL.Keyword (Keyword (..))
