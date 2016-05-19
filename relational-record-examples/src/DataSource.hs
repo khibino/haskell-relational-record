b946fd94ed3fdc57d7a045c6c0f527f26d044e27
Kei Hibino <ex8k.hibino@gmail.com>
2016-05-19 09:44:52 +0900

    Use default mapping.

--- relational-record-examples/src/DataSource.hs
+++ relational-record-examples/src/DataSource.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module DataSource (
-    connect, convTypes, defineTable
+    connect, defineTable
   ) where
 
-import Data.Time (Day, LocalTime)
 import Database.HDBC.Query.TH (defineTableFromDB)
 import Database.HDBC.Schema.Driver (typeMap)
 import Database.HDBC.Schema.SQLite3 (driverSQLite3)
@@ -14,20 +13,11 @@ import Language.Haskell.TH (Q, Dec, TypeQ)
 connect :: IO Connection
 connect = connectSqlite3 "examples.db"
 
-convTypes :: [(String, TypeQ)]
-convTypes =
-    [ ("float", [t|Double|])
-    , ("date", [t|Day|])
-    , ("datetime", [t|LocalTime|])
-    , ("double", [t|Double|])
-    , ("varchar", [t|String|])
-    ]
-
 defineTable :: String -> Q [Dec]
 defineTable tableName =
   defineTableFromDB
     connect
-    (driverSQLite3 { typeMap = convTypes }) -- overwrite the default type map with yours
+    (driverSQLite3 { typeMap = [("FLOAT", [t|Double|])] }) -- overwrite the default type map with yours
     "main" -- schema name, ignored by SQLite
     tableName
     [''Show]
