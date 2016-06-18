bd00c269be81d0b2e4eaf965177f732de8dddc1e
Kei Hibino <ex8k.hibino@gmail.com>
2016-06-18 22:36:49 +0900

    drop old scripts.

--- travis-CI/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-
-cabal install cabal-test-compat
-cd relational-query
-cabal install --only-dependencies
--- travis-CI/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -x
-
-cd relational-query
-./cabal-test.sh
