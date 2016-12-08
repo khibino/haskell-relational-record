0bc173d62a7a4bfa36ac6cb4eed4a136070cdecc
Kei Hibino <ex8k.hibino@gmail.com>
2016-12-08 16:57:40 +0900

    add derived table and show instances for tests.

--- rr-quickcheck/src/Test/Relational/QuickCheck/Model.hs
+++ rr-quickcheck/src/Test/Relational/QuickCheck/Model.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, FlexibleInstances #-}
 
 module Test.Relational.QuickCheck.Model (
-  A (..), a0', a1', a2',
-  B (..), b0', b1', b2',
+  A (..), a0', a1', a2', relA,
+  B (..), b0', b1', b2', relB,
   ) where
 
 import Data.Int (Int64)
+import qualified Database.Relational.Query.Table as Table
+import Database.Relational.Query (Relation, table, TableDerivable (derivedTable))
 import Database.HDBC.Query.TH (makeRecordPersistableDefault)
 
 
@@ -14,15 +16,27 @@ data A =
   { a0 :: Int64
   , a1 :: Int64
   , a2 :: Int64
-  } deriving (Eq, Show)
+  } deriving (Eq, Ord, Show)
 
 data B =
   B
   { b0 :: Int64
   , b1 :: Int64
   , b2 :: Int64
-  } deriving (Eq, Show)
+  } deriving (Eq, Ord, Show)
 
 
 $(makeRecordPersistableDefault ''A)
 $(makeRecordPersistableDefault ''B)
+
+instance TableDerivable A where
+  derivedTable = Table.table "ARBITRARY0.A" ["a0", "a1", "a2"]
+
+relA :: Relation () A
+relA = table derivedTable
+
+instance TableDerivable B where
+  derivedTable = Table.table "ARBITRARY0.B" ["b0", "b1", "b2"]
+
+relB :: Relation () B
+relB = table derivedTable
