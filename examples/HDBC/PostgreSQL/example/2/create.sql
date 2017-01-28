01acb337f100854fd9d5b878580054fbd747c364
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-28 16:38:59 +0900

    example PostgreSQL: switch from sh to SQL.

--- examples/HDBC/PostgreSQL/example/2/create.sql
+++ examples/HDBC/PostgreSQL/example/2/create.sql
@@ -1,18 +1,11 @@
-#! /bin/sh
+CREATE SCHEMA EXAMPLE2;
 
-create_keytest_table='
 CREATE TABLE EXAMPLE2.keytest (
  name VARCHAR(30) NOT NULL,
  bar INTEGER NOT NULL,
  foo INTEGER NOT NULL,
  attr VARCHAR(30) NOT NULL,
  baz INTEGER NOT NULL,
- 
+
  PRIMARY KEY(foo, name, bar)
-)
-'
-
-set -x
-
-psql -c "CREATE SCHEMA EXAMPLE2" testdb
-psql -c "$create_keytest_table" testdb
+);
--- examples/HDBC/PostgreSQL/example/2/drop.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-set -x
-
-psql -c "DROP TABLE EXAMPLE2.keytest" testdb
-psql -c "DROP SCHEMA IF EXISTS EXAMPLE2" testdb
