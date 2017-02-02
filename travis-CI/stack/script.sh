272876725997f1793e9103bef8b6961e85d5db11
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-02 17:46:18 +0900

    travis-ci: drop parallel switch. drop coverage.

--- travis-CI/stack/script.sh
+++ travis-CI/stack/script.sh
@@ -9,8 +9,6 @@ set -x
 script_build() {
     STACK_YAML=stack-travis.yaml stack build
     STACK_YAML=stack-travis.yaml stack test
-    ## '--coverage' may change result of fail or success
-    STACK_YAML=stack-travis.yaml stack test --coverage
 }
 
 if [ x"$dirs" = x ]; then
