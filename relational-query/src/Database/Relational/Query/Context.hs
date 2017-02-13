8054d493d1645e3b2f7863bcc8e6e6229aa781ba
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-13 15:10:26 +0900

    relational-query: move context type definitions to internal space.

--- relational-query/src/Database/Relational/Query/Context.hs
+++ relational-query/src/Database/Relational/Query/Context.hs
@@ -1,39 +1,15 @@
-{-# LANGUAGE EmptyDataDecls #-}
-
 -- |
 -- Module      : Database.Relational.Query.Context
--- Copyright   : 2013 Kei Hibino
+-- Copyright   : 2017 Kei Hibino
 -- License     : BSD3
 --
 -- Maintainer  : ex8k.hibino@gmail.com
 -- Stability   : experimental
 -- Portability : unknown
 --
--- This module defines query context tag types.
-module Database.Relational.Query.Context (
-  Flat, Aggregated, Exists, OverWindow,
+-- This module re-export query context tag types.
+module Database.Relational.Query.Context
+       ( module Database.Relational.Query.Internal.ContextType
+       ) where
 
-  Set, SetList, Power,
-  ) where
-
--- | Type tag for flat (not-aggregated) query
-data Flat
-
--- | Type tag for aggregated query
-data Aggregated
-
--- | Type tag for exists predicate
-data Exists
-
--- | Type tag for window function building
-data OverWindow
-
-
--- | Type tag for normal aggregatings set
-data Set
-
--- | Type tag for aggregatings GROUPING SETS
-data SetList
-
--- | Type tag for aggregatings power set
-data Power
+import Database.Relational.Query.Internal.ContextType
