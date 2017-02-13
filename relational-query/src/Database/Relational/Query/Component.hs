de7fa4c874cf58dd3e3603ea8a4a7d4f1f435680
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-13 20:06:56 +0900

    relational-query: move configuration definitions to internal space.

--- relational-query/src/Database/Relational/Query/Component.hs
+++ relational-query/src/Database/Relational/Query/Component.hs
@@ -18,16 +18,7 @@ module Database.Relational.Query.Component
          ColumnSQL, columnSQL, columnSQL', showsColumnSQL,
 
          -- * Configuration type for query
-         NameConfig (..),
-         ProductUnitSupport (..), SchemaNameMode (..), IdentifierQuotation (..),
-         Config ( productUnitSupport
-                , chunksInsertSize
-                , schemaNameMode
-                , normalizedTableName
-                , verboseAsCompilerWarning
-                , identifierQuotation
-                , nameConfig),
-         defaultConfig,
+         module Database.Relational.Query.Internal.Config,
 
          -- * Duplication attribute
          Duplication (..), showsDuplication,
@@ -60,9 +51,11 @@ import Data.Monoid (Monoid (..), (<>))
 
 import Language.SQL.Keyword (Keyword(..), (|*|), (.=.))
 import qualified Language.SQL.Keyword as SQL
-import Language.Haskell.TH.Name.CamelCase (VarName, varCamelcaseName)
-import qualified Database.Record.TH as RecordTH
 
+import Database.Relational.Query.Internal.Config
+  (NameConfig (..),
+   ProductUnitSupport (..), SchemaNameMode (..), IdentifierQuotation (..),
+   Config (..), defaultConfig,)
 import Database.Relational.Query.Internal.SQL (StringSQL, rowConsStringSQL)
 import qualified Database.Relational.Query.Internal.SQL as Internal
 
@@ -83,53 +76,6 @@ columnSQL' = Internal.columnSQL'
 showsColumnSQL :: ColumnSQL -> StringSQL
 showsColumnSQL = Internal.showsColumnSQL
 
--- | 'NameConfig' type to customize names of expanded templates.
-data NameConfig =
-  NameConfig
-  { recordConfig       ::  RecordTH.NameConfig
-  , relationVarName    ::  String -> String -> VarName
-  }
-
-instance Show NameConfig where
-  show = const "<NameConfig>"
-
--- | Unit of product is supported or not.
-data ProductUnitSupport = PUSupported | PUNotSupported  deriving Show
-
--- | Schema name qualify mode in SQL string.
-data SchemaNameMode
-  = SchemaQualified     -- ^ Schema qualified table name in SQL string
-  | SchemaNotQualified  -- ^ Not qualified table name in SQL string
-  deriving (Eq, Show)
-
--- | Configuration for quotation of identifiers of SQL.
-data IdentifierQuotation = NoQuotation | Quotation Char deriving Show
-
--- | Configuration type.
-data Config =
-  Config
-  { productUnitSupport        ::  !ProductUnitSupport
-  , chunksInsertSize          ::  !Int
-  , schemaNameMode            ::  !SchemaNameMode
-  , normalizedTableName       ::  !Bool
-  , verboseAsCompilerWarning  ::  !Bool
-  , identifierQuotation       ::  !IdentifierQuotation
-  , nameConfig                ::  !NameConfig
-  } deriving Show
-
--- | Default configuration.
-defaultConfig :: Config
-defaultConfig =
-  Config { productUnitSupport        =  PUSupported
-         , chunksInsertSize          =  256
-         , schemaNameMode            =  SchemaQualified
-         , normalizedTableName       =  True
-         , verboseAsCompilerWarning  =  False
-         , identifierQuotation       =  NoQuotation
-         , nameConfig                =  NameConfig { recordConfig     =  RecordTH.defaultNameConfig
-                                                   , relationVarName  =  const varCamelcaseName
-                                                   }
-         }
 
 -- | Result record duplication attribute
 data Duplication = All | Distinct  deriving Show
