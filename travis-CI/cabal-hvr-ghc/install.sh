9a004131a832496eebd66aaf9df35ae82f4bbb05
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-22 07:53:08 +0900

    travis-ci: simplify.

--- travis-CI/cabal-hvr-ghc/install.sh
+++ travis-CI/cabal-hvr-ghc/install.sh
@@ -10,18 +10,6 @@ set -x
 cabal --version
 echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
 
-## In package list cache hit case, cabal install may fail
-(
-    if [ -d $HOME/.cabal/packages/hackage.haskell.org/ ]; then
-        cd $HOME/.cabal/packages/hackage.haskell.org/
-        rm -f \
-           00-index.tar.gz.etag \
-           00-index.tar.gz \
-           00-index.tar \
-           00-index.cache
-    fi
-)
-
 custom_retry cabal update -v
 sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
 
