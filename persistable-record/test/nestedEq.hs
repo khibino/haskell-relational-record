c89b83edb3d6085f0572d9a150c9b3aaeaa61cac
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-08 13:00:50 +0900

    add test case for null record.

--- persistable-record/test/nestedEq.hs
+++ persistable-record/test/nestedEq.hs
@@ -12,8 +12,15 @@ main :: IO ()
 main =
   defaultMain
   [ eqTest
-    "nestedEq"
+    "nested just"
     (Membership { user  = User { uid = 1, uname = "Kei Hibino", note = "HRR developer" }
                 , group = Just $ Group { gid = 1, gname = "Haskellers" }
                 } )
-    (toRecord ["1", "Kei Hibino", "HRR developer", "1", "Haskellers"]) ]
+    (toRecord ["1", "Kei Hibino", "HRR developer", "1", "Haskellers"])
+  , eqTest
+    "nested nothing"
+    (Membership { user  = User { uid = 1, uname = "Kei Hibino", note = "HRR developer" }
+                , group = Nothing
+                } )
+    (toRecord ["1", "Kei Hibino", "HRR developer", "<null>", "<null>"])
+  ]
