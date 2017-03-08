b9a6336920634d8575c1a942f2bea2a716e9bf2a
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-08 15:07:50 +0900

    add ToSql instances for tests.

--- persistable-record/test/Model.hs
+++ persistable-record/test/Model.hs
@@ -5,11 +5,11 @@ module Model where
 import Control.Applicative ((<$>), (<*>))
 
 import Database.Record
-  (PersistableType (..),
+  (PersistableType (..), PersistableWidth (..),
    FromSql (..), valueRecordFromSql,
-   ToSql (..), valueRecordToSql)
+   ToSql (..), valueRecordToSql, wrapToSql, putRecord)
 import Database.Record.KeyConstraint (HasColumnConstraint (..), NotNull, unsafeSpecifyColumnConstraint)
-import Database.Record.Persistable (unsafePersistableSqlTypeFromNull)
+import Database.Record.Persistable (unsafePersistableSqlTypeFromNull, unsafePersistableRecordWidth)
 
 
 instance PersistableType String where
@@ -54,6 +54,12 @@ instance HasColumnConstraint NotNull User where
 instance HasColumnConstraint NotNull Group where
   columnConstraint = unsafeSpecifyColumnConstraint 0
 
+instance PersistableWidth User where
+  persistableWidth = unsafePersistableRecordWidth 3
+
+instance PersistableWidth Group where
+  persistableWidth = unsafePersistableRecordWidth 2
+
 instance FromSql String User where
   recordFromSql = User <$> recordFromSql <*> recordFromSql <*> recordFromSql
 
@@ -62,3 +68,19 @@ instance FromSql String Group where
 
 instance FromSql String Membership where
   recordFromSql = Membership <$> recordFromSql <*> recordFromSql
+
+instance ToSql String User where
+  recordToSql = wrapToSql $ \u -> do
+    putRecord $ uid u
+    putRecord $ uname u
+    putRecord $ note u
+
+instance ToSql String Group where
+  recordToSql = wrapToSql $ \g -> do
+    putRecord $ gid g
+    putRecord $ gname g
+
+instance ToSql String Membership where
+  recordToSql = wrapToSql $ \m -> do
+    putRecord $ user m
+    putRecord $ group m
