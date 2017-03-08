3540f1c05390be879aeaee63049d590d24738e8d
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-08 15:08:05 +0900

    add fromRecord tests.

--- persistable-record/test/nestedEq.hs
+++ persistable-record/test/nestedEq.hs
@@ -3,7 +3,7 @@
 
 import Test.QuickCheck.Simple (defaultMain, eqTest)
 
-import Database.Record (toRecord)
+import Database.Record (toRecord, fromRecord)
 
 import Model (User (..), Group (..), Membership (..))
 
@@ -12,15 +12,27 @@ main :: IO ()
 main =
   defaultMain
   [ eqTest
-    "nested just"
+    "toRecord just"
     (Membership { user  = User { uid = 1, uname = "Kei Hibino", note = "HRR developer" }
                 , group = Just $ Group { gid = 1, gname = "Haskellers" }
                 } )
     (toRecord ["1", "Kei Hibino", "HRR developer", "1", "Haskellers"])
   , eqTest
-    "nested nothing"
+    "toRecord nothing"
     (Membership { user  = User { uid = 1, uname = "Kei Hibino", note = "HRR developer" }
                 , group = Nothing
                 } )
     (toRecord ["1", "Kei Hibino", "HRR developer", "<null>", "<null>"])
+  , eqTest
+    "fromRecord just"
+    (fromRecord $ Membership { user  = User { uid = 1, uname = "Kei Hibino", note = "HRR developer" }
+                             , group = Just $ Group { gid = 1, gname = "Haskellers" }
+                             } )
+    ["1", "Kei Hibino", "HRR developer", "1", "Haskellers"]
+  , eqTest
+    "fromRecord note"
+    (fromRecord $ Membership { user  = User { uid = 1, uname = "Kei Hibino", note = "HRR developer" }
+                             , group = Nothing
+                             } )
+    ["1", "Kei Hibino", "HRR developer", "<null>", "<null>"]
   ]
