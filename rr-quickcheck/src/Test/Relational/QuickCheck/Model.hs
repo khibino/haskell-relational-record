5dbbcee027152f8adadac978b371ec2b27c3aa3b
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-16 16:06:04 +0900

    apply DeriveGeneric.

--- rr-quickcheck/src/Test/Relational/QuickCheck/Model.hs
+++ rr-quickcheck/src/Test/Relational/QuickCheck/Model.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, FlexibleInstances, DeriveGeneric #-}
 
 module Test.Relational.QuickCheck.Model (
   A (..), a0', a1', a2', relA,
   B (..), b0', b1', b2', relB,
   ) where
 
+import GHC.Generics (Generic)
 import Data.Int (Int64)
 import qualified Database.Relational.Query.Table as Table
 import Database.Relational.Query (Relation, table, TableDerivable (derivedTable))
@@ -16,14 +17,14 @@ data A =
   { a0 :: Int64
   , a1 :: Int64
   , a2 :: Int64
-  } deriving (Eq, Ord, Show)
+  } deriving (Eq, Ord, Show, Generic)
 
 data B =
   B
   { b0 :: Int64
   , b1 :: Int64
   , b2 :: Int64
-  } deriving (Eq, Ord, Show)
+  } deriving (Eq, Ord, Show, Generic)
 
 
 $(makeRecordPersistableDefault ''A)
