07b4ba68d6d5e09392f57ee8d06391f58c0724ba
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-17 11:27:16 +0900

    examples/HDBC/MySQL: apply DeriveGeneric.

--- examples/HDBC/MySQL/src/Example/User.hs
+++ examples/HDBC/MySQL/src/Example/User.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Example.User where
 
+import GHC.Generics (Generic)
 import Prelude hiding (id)
 import Example.DataSource (defineTable)
 
 $(defineTable
     []
-    "TEST" "user" [])
+    "TEST" "user" [''Generic])
