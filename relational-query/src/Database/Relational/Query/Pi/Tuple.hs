f94d8a0bd45a9f8e2311392f52186704b842b1ad
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-23 13:06:41 +0900

    relational-query: add haddock header.

--- relational-query/src/Database/Relational/Query/Pi/Tuple.hs
+++ relational-query/src/Database/Relational/Query/Pi/Tuple.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+-- |
+-- Module      : Database.Relational.Query.Pi.Tuple
+-- Copyright   : 2017 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines projection path objects for tuple types.
 module Database.Relational.Query.Pi.Tuple where
 
 import Control.Applicative ((<$>))
