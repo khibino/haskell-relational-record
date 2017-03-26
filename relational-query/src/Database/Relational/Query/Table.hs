71861dce9265f36809928b5a39a4ead300e7bd06
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-27 00:06:19 +0900

    relational-query: drop old deprecated interfaces. (grafted from 403d3de3f38beefebfb0c9f9e00c5b6b3c3211dc)

--- relational-query/src/Database/Relational/Query/Table.hs
+++ relational-query/src/Database/Relational/Query/Table.hs
@@ -9,10 +9,6 @@
 --
 -- This module defines table type which has table metadatas.
 module Database.Relational.Query.Table (
-  -- * Untyped table type
-  -- deprecated interfaces
-  Untyped, name', width', columns', (!),
-
   -- * Phantom typed table type
   Table, unType, name, shortName, width, columns, index, table, toMaybe,
 
@@ -24,29 +20,9 @@ import Data.Array (listArray)
 
 import Database.Record (PersistableWidth)
 
-import qualified Database.Relational.Query.Internal.UntypedTable as Untyped
+import Database.Relational.Query.Internal.UntypedTable (Untyped (Untyped), name', width', columns', (!))
 import Database.Relational.Query.Internal.SQL (ColumnSQL, columnSQL)
 
-
-{-# DEPRECATED Untyped, name', width', columns', (!) "prepare to drop public interface. internally use Database.Relational.Query.Internal.UntypedTable.*" #-}
--- | Untyped typed table type
-type Untyped = Untyped.Untyped
-
--- | Name string of table in SQL
-name' :: Untyped -> String
-name' = Untyped.name'
-
--- | Width of table
-width' :: Untyped -> Int
-width' = Untyped.width'
-
--- | Column name strings in SQL
-columns' :: Untyped -> [ColumnSQL]
-columns' = Untyped.columns'
-
--- | Column name strings in SQL
-(!) :: Untyped -> Int -> ColumnSQL
-(!) = (Untyped.!)
 
 -- | Phantom typed table type
 newtype Table r = Table Untyped
@@ -57,7 +33,7 @@ unType (Table u) = u
 
 -- | Name string of table in SQL
 name :: Table r -> String
-name   = name'   . unType
+name   = name' . unType
 
 -- | Not qualified name string of table in SQL
 shortName :: Table r -> String
@@ -65,7 +41,7 @@ shortName =  tail . dropWhile (/= '.') . name
 
 -- | Width of table
 width :: Table r -> Int
-width  = width'  . unType
+width  = width' . unType
 
 -- | Column name strings in SQL
 columns :: Table r -> [ColumnSQL]
@@ -83,7 +59,7 @@ toMaybe (Table t) = Table t
 
 -- | Unsafely generate phantom typed table type.
 table :: String -> [String] -> Table r
-table n f = Table $ Untyped.Untyped n w fa  where
+table n f = Table $ Untyped n w fa  where
   w  = length f
   fa = listArray (0, w - 1) $ map columnSQL f
 
