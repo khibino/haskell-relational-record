9a59f137d55744a89eaee9791eedd624753ddb23
Kei Hibino <ex8k.hibino@gmail.com>
2017-03-28 12:03:16 +0900

    relational-query: update haddock.

--- relational-query/src/Database/Relational/Query/BaseTH.hs
+++ relational-query/src/Database/Relational/Query/BaseTH.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleInstances #-}
 
+-- |
+-- Module      : Database.Relational.Query.BaseTH
+-- Copyright   : 2017 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module defines templates for internally using.
 module Database.Relational.Query.BaseTH (
   defineProductConstructorInstance,
   defineTupleProductConstructor,
