e93a326739e477bcb18d6b2ab9bc16502218d32c
Kei Hibino <ex8k.hibino@gmail.com>
2017-04-10 17:48:07 +0900

    add prototype codes of sequence typing.

---
+++ proto/Sequence.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+-- |
+-- Module      : Sequence
+-- Copyright   : 2017 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module provides sequence model for relational-record.
+module Sequence (
+  Sequence (..), relation,
+  unsafeSpecifySequence,
+
+  SequenceDerivable (..),
+
+  SequenceFromTable (..), fromRelation,
+
+  Number, unsafeSpecifyNumber, unsafeExtractNumber,
+  ($$!), ($$),
+
+  updateNumber,
+  ) where
+
+import Prelude hiding (seq)
+
+import Database.Relational.Query
+  (ShowConstantTermsSQL, updateTarget', Update, typedUpdate,
+   Pi, TableDerivable, derivedTable, tableOf, Table, Relation,
+   (<-#), (.<=.), (!), wheres, unitPlaceHolder, value, )
+import qualified Database.Relational.Query as Relational
+
+
+data Sequence s i =
+  Sequence
+  { table :: Table s
+  , extract :: s -> i
+  , key :: Pi s i
+  }
+
+-- | Unsafely specify sequence table.
+unsafeSpecifySequence :: TableDerivable s => (s -> i) -> Pi s i -> Sequence s i
+unsafeSpecifySequence = Sequence derivedTable
+
+relation :: TableDerivable s => Sequence s i -> Relation () s
+relation = Relational.table . table
+
+class TableDerivable s => SequenceDerivable s i | s -> i where
+  deriveSequence :: Sequence s i
+
+class (TableDerivable r, SequenceDerivable s i)
+      => SequenceFromTable r s i | r -> s  where
+  fromTable :: Table r -> Sequence s i
+  fromTable = const deriveSequence
+
+fromRelation :: SequenceFromTable r s i
+             => Relation () r
+             -> Sequence s i
+fromRelation = fromTable . tableOf
+
+newtype Number r i = Number i deriving (Eq, Ord, Show)
+
+-- | Unsafely specify sequence number.
+unsafeSpecifyNumber :: SequenceFromTable r s i => i -> Number r i
+unsafeSpecifyNumber = Number
+
+unsafeExtractNumber :: Number r i -> i
+unsafeExtractNumber (Number i) = i
+
+-- | Unsafely apply sequence number.
+($$!) :: (i -> r) -> Number r i -> r
+($$!) = (. unsafeExtractNumber)
+
+-- | Unsafely apply sequence number. Only safe to build corresponding record type.
+($$) :: SequenceFromTable r s i => (i -> r) -> Number r i -> r
+($$) = ($$!)
+
+{-
+updateNumber :: PersistableWidth p => Sequence r p -> Update (p, p)
+updateNumber seqt = typedUpdate (Sequence.table seqt) . updateTarget' $ \ proj -> do
+  (phv', ()) <- placeholder (\ph -> Sequence.key seqt <-# ph)
+  (phx', ()) <- placeholder (\ph -> wheres $ proj ! key seqt .<=. ph)
+  return $ (,) |$| phv' |*| phx'
+ -}
+
+updateNumber :: (Integral i, ShowConstantTermsSQL i)
+             => i            -- ^ sequence number to set. expect not SQL injectable.
+             -> Sequence s i -- ^ sequence table
+             -> Update ()
+updateNumber i seqt = typedUpdate (Sequence.table seqt) . updateTarget' $ \ proj -> do
+  let iv = value i
+  Sequence.key seqt <-# iv
+  wheres $ proj ! key seqt .<=. iv -- fool proof
+  return unitPlaceHolder
---
+++ proto/SequenceHDBC.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+-- |
+-- Module      : SequenceHDBC
+-- Copyright   : 2017 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module provides sequence operations of relational-record for HDBC.
+module SequenceHDBC (
+  pool', pool, autoPool,
+  ) where
+
+import Control.Applicative ((<$>))
+import Control.Monad (when, void)
+import System.IO.Unsafe (unsafeInterleaveIO)
+import Database.HDBC (IConnection, SqlValue, commit)
+import Database.HDBC.Session (withConnectionIO)
+
+import Language.SQL.Keyword (Keyword (FOR, UPDATE))
+import Database.Record (FromSql, ToSql, PersistableWidth)
+import Database.Relational.Query
+  (relationalQuery', ShowConstantTermsSQL, Relation, )
+import qualified Database.Relational.Query as Relation
+import qualified Database.Relational.Query.Table as Table
+import Database.HDBC.Record.Persistable ()
+import Database.HDBC.Record.Statement (bind, execute)
+import Database.HDBC.Record.Query (prepareQuery, fetch)
+import Database.HDBC.Record.Update (runUpdate)
+
+import Sequence (Sequence, SequenceFromTable, Number, )
+import qualified Sequence
+
+
+unsafePool :: (FromSql SqlValue s, ToSql SqlValue i,
+               PersistableWidth i, ShowConstantTermsSQL i,
+               Bounded i, Integral i, Show i, IConnection conn)
+           => conn
+           -> i
+           -> Sequence s i
+           -> IO [i]
+unsafePool conn sz seqt = do
+  let t      = Sequence.table seqt
+      name   = Table.name t
+  pq    <- prepareQuery conn $ relationalQuery' (Relation.table t) [FOR, UPDATE]
+
+  es    <- execute $ pq `bind` ()
+  seq0  <- maybe
+           (fail $ "No record found in sequence table: " ++ name)
+           (return . Sequence.extract seqt)
+           =<< fetch es
+  when (maxBound - seq0 < sz) . fail
+    $ "Not enough size in sequence table: "
+    ++ name ++ ": " ++ show (maxBound - seq0) ++ " < " ++ show sz
+
+  let seq1 = seq0 + sz
+  void $ runUpdate conn (Sequence.updateNumber seq1 seqt) ()
+  maybe (return ()) (const . fail $ "More than two record found in seq table: " ++ name) =<< fetch es
+
+  commit conn
+  return [seq0 + 1 .. seq1]
+
+pool' :: (FromSql SqlValue s, ToSql SqlValue i,
+          PersistableWidth i, ShowConstantTermsSQL i,
+          Bounded i, Integral i, Show i, IConnection conn,
+          SequenceFromTable r s i)
+      => conn
+      -> i
+      -> Relation () r
+      -> IO [Number r i]
+pool' conn sz = (map Sequence.unsafeSpecifyNumber <$>) . unsafePool conn sz . Sequence.fromRelation
+
+pool :: (FromSql SqlValue s, ToSql SqlValue i,
+         PersistableWidth i, ShowConstantTermsSQL i,
+         Bounded i, Integral i, Show i, IConnection conn,
+         SequenceFromTable r s i)
+     => IO conn
+     -> i
+     -> Relation () r
+     -> IO [Number r i]
+pool connAct sz seqt = withConnectionIO connAct $ \conn -> pool' conn sz seqt
+
+autoPool :: (FromSql SqlValue s, ToSql SqlValue i,
+             PersistableWidth i, ShowConstantTermsSQL i,
+             Bounded i, Integral i, Show i, IConnection conn,
+             SequenceFromTable r s i)
+         => i
+         -> IO conn
+         -> Relation () r
+         -> IO [Number r i]
+autoPool sz connAct seqt = loop  where
+  loop = unsafeInterleaveIO $ do
+    hd <- pool connAct sz seqt
+    tl <- loop
+    return $ hd ++ tl
