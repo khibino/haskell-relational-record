2472892bf1828c015f37ab03292d6b4068bf7e10
Kei Hibino <ex8k.hibino@gmail.com>
2017-07-16 15:29:28 +0900

    travis-CI: switch to make_travis_yml_2 and apply skip_no_match_branch.

---
+++ travis-CI/cabal-hvr-ghc-2/install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+. ./travis-CI/sh-lib
+. ./travis-CI/custom-cabal
+. ./travis-CI/dirs.list
+
+set -x
+
+cabal --version
+echo "$(${HC} --version) [$(${HC} --print-project-git-commit-id 2> /dev/null || echo '?')]"
+
+BENCH=${BENCH---enable-benchmarks}
+TEST=${TEST---enable-tests}
+
+gen_custom_cabal_config
+custom_retry cabal update -v
+sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
+
+install_package() {
+    rm -fv cabal.project.local
+    echo 'packages: .' > cabal.project
+    rm -f cabal.project.freeze
+    cabal new-build -w ${HC} ${TEST} ${BENCH} --dep -j2 all
+    cabal new-build -w ${HC} --disable-tests --disable-benchmarks --dep -j2 all
+}
+
+if [ x"$dirs" = x ]; then
+    install_package
+else
+    for d in $dirs; do
+        ( cd $d && install_package )
+    done
+fi
--- travis-CI/stack/before-install.sh
+++ travis-CI/stack/before-install.sh
@@ -6,6 +6,8 @@ set -e
 
 set -x
 
+skip_no_match_branch
+
 mkdir -p ~/.local/bin
 custom_retry curl -L https://www.stackage.org/stack/linux-x86_64 \
     | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
