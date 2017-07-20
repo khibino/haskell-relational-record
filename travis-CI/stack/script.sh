050fe49f29fc7550adbce173cb44360c5148dd7d
Kei Hibino <ex8k.hibino@gmail.com>
2017-07-21 01:35:03 +0900

    travis-CI/stack: switch stack build, not building each directory.

--- travis-CI/stack/script.sh
+++ travis-CI/stack/script.sh
@@ -9,15 +9,5 @@ set -x
 
 skip_no_match_branch
 
-script_build() {
-    STACK_YAML=stack-travis.yaml stack build
-    STACK_YAML=stack-travis.yaml stack test
-}
-
-if [ x"$dirs" = x ]; then
-    script_build
-else
-    for d in $dirs; do
-        ( cd $d && script_build )
-    done
-fi
+STACK_YAML=stack-travis.yaml stack build
+STACK_YAML=stack-travis.yaml stack test
