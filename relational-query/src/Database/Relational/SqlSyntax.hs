fa4b1c53e8ee521d7fabcf710c9773efd2068e40
Kei Hibino <ex8k.hibino@gmail.com>
2017-08-15 22:44:47 +0900

    relational-query: add integrated SqlSyntax namespace.

---
+++ relational-query/src/Database/Relational/SqlSyntax.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      : Database.Relational.SqlSyntax
+-- Copyright   : 2017 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module is integrated module of sql-syntax.
+module Database.Relational.SqlSyntax (
+  module Database.Relational.SqlSyntax.Types,
+  ) where
+
+import Database.Relational.SqlSyntax.Types
