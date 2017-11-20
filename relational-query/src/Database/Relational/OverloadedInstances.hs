81b24e751bafbfb062bd3280e98ba4f4df5d8115
Kei Hibino <ex8k.hibino@gmail.com>
2017-11-21 00:00:08 +0900

    relational-query: apply overloaded tuple projection template.

---
+++ relational-query/src/Database/Relational/OverloadedInstances.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+
+module Database.Relational.OverloadedInstances () where
+
+import Control.Applicative ((<$>))
+import Database.Record.Persistable
+  (PersistableWidth, persistableWidth,
+   PersistableRecordWidth, runPersistableRecordWidth)
+
+import Database.Relational.Pi.Unsafe (definePi)
+#if __GLASGOW_HASKELL__ >= 800
+import Database.Relational.OverloadedProjection (HasProjection (projection))
+#endif
+import Database.Relational.InternalTH.Overloaded (tupleProjection)
+
+
+$(concat <$> mapM tupleProjection [2 .. 7])
+-- Generic instances of tuple types are generated from 2 to 7 in GHC.Generics.
+
+instance PersistableWidth a =>
+         HasProjection "fst" (a, b) a where
+  projection _ = definePi 0
+
+instance (PersistableWidth a, PersistableWidth b) =>
+         HasProjection "snd" (a, b) b where
+  projection _ = definePi $ runPersistableRecordWidth (persistableWidth :: PersistableRecordWidth b)
