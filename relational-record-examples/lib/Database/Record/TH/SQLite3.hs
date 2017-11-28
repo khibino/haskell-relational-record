ba21bb4a765452c8509a5760f4061e22d4484079
Kei Hibino <ex8k.hibino@gmail.com>
2017-11-28 20:23:38 +0900

    relational-record-examples: compatible configuration with olders.

--- relational-record-examples/lib/Database/Record/TH/SQLite3.hs
+++ relational-record-examples/lib/Database/Record/TH/SQLite3.hs
@@ -5,19 +5,21 @@ module Database.Record.TH.SQLite3 (
   ) where
 
 import GHC.Generics (Generic)
-import Database.Relational (Config(..), defaultConfig)
-import Database.HDBC.Query.TH (defineTableFromDB')
-import Database.HDBC.Schema.Driver (typeMap)
+import Database.Relational (Config(..))
+import Database.HDBC.Query.TH (defineTableFromDB)
+import Database.HDBC.Schema.Driver (driverConfig, typeMap)
 import Database.HDBC.Schema.SQLite3 (driverSQLite3)
 import Database.HDBC.Sqlite3 (connectSqlite3)
 import Language.Haskell.TH (Q, Dec)
 
 defineTable :: FilePath -> String -> Q [Dec]
 defineTable fileName tableName =
-  defineTableFromDB'
+  defineTableFromDB
     (connectSqlite3 fileName)
-    defaultConfig { disableOverloadedProjection = True }
-    (driverSQLite3 { typeMap = [("FLOAT", [t|Double|]), ("INTEGER", [t|Int|])] }) -- overwrite the default type map with yours
+    (drv { typeMap = [("FLOAT", [t|Double|]), ("INTEGER", [t|Int|])] -- overwrite the default type map with yours
+         , driverConfig = (driverConfig drv) { disableOverloadedProjection = True }
+         })
     "main" -- schema name, ignored by SQLite
     tableName
     [''Show, ''Generic]
+  where drv = driverSQLite3
