5a8172c80abd3f225da9251b191534739a3099ea
Kei Hibino <ex8k.hibino@gmail.com>
2018-04-27 23:39:28 +0900

    travis-CI: update hvr-ghc build scripts.

--- travis-CI/cabal-hvr-ghc-2/install.sh
+++ travis-CI/cabal-hvr-ghc-2/install.sh
@@ -17,13 +17,18 @@ TEST=${TEST---enable-tests}
 gen_custom_cabal_config
 custom_retry cabal update -v
 sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
+grep -Ev -- '^\s*--' ${HOME}/.cabal/config | grep -Ev '^\s*$'
 
 install_package() {
     rm -fv cabal.project.local
     echo 'packages: .' > cabal.project
+    cat cabal.project
+    if [ -f configure.ac ]; then autoreconf -i; fi
     rm -f cabal.project.freeze
     cabal new-build -w ${HC} ${TEST} ${BENCH} --dep -j2 all
     cabal new-build -w ${HC} --disable-tests --disable-benchmarks --dep -j2 all
+
+    rm -rf .ghc.environment.* dist/
 }
 
 if [ x"$dirs" = x ]; then
--- travis-CI/cabal-hvr-ghc-2/script.sh
+++ travis-CI/cabal-hvr-ghc-2/script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,29 +6,51 @@ set -e
 
 set -x
 
+BENCH=${BENCH---enable-benchmarks}
+TEST=${TEST---enable-tests}
+HADDOCK=${HADDOCK-true}
+INSTALLED=${INSTALLED-true}
+
+## ghc-pkg path. ## substitution of bash
+HCPKG=${HC/ghc/ghc-pkg}
+
 script_build() {
     PKGNAME="$1"
 
-    if [ -f configure.ac ]; then autoreconf -i; fi
-    rm -rf .ghc.environment.* dist/
+    DISTDIR=$(mktemp -d /tmp/dist-test.XXXX)
     cabal sdist # test that a source-distribution can be generated
-    cd dist/
-    SRCTAR=$(echo ${PKGNAME}-*.tar.gz)
-    SRC_BASENAME="${SRCTAR%.tar.gz}"
-    tar -xvf "./${SRC_BASENAME}.tar.gz"
-    cd "$SRC_BASENAME/"
-    ## from here on, CWD is inside the extracted source-tarball
-    rm -fv cabal.project.local
-    echo 'packages: .' > cabal.project
-    # this builds all libraries and executables (without tests/benchmarks)
-    rm -f cabal.project.freeze
-    cabal new-build -w ${HC} --disable-tests --disable-benchmarks all
-    # this builds all libraries and executables (including tests/benchmarks)
-    # - rm -rf ./dist-newstyle
+    mv "."/dist/${PKGNAME}-*.tar.gz ${DISTDIR}/
+    cd ${DISTDIR} || false
+    find . -maxdepth 1 -name '*.tar.gz' -exec tar -xvf '{}' \;
 
-    # build & run tests
+    printf "packages: ${PKGNAME}-"*/*'.cabal\n' > cabal.project
+    cat cabal.project
+
+    # this builds all libraries and executables (without tests/benchmarks)
+    cabal new-build -w ${HC} --disable-tests --disable-benchmarks all
+
+    # Build with installed constraints for packages in global-db
+    if $INSTALLED; then
+        echo cabal new-build -w ${HC} --disable-tests --disable-benchmarks $(${HCPKG} list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh;
+    else
+        echo "Not building with installed constraints";
+    fi
+
+    # build & run tests, build benchmarks
     cabal new-build -w ${HC} ${TEST} ${BENCH} all
-    if [ "x$TEST" = "x--enable-tests" ]; then cabal new-test -w ${HC} ${TEST} all; fi
+    for p in $test_pkgs ; do
+        if [ x$PKGNAME = x$p ]; then
+            cabal new-test -w ${HC} ${TEST} ${BENCH} all
+            break
+        fi
+    done
+
+    # cabal check
+    (cd ${PKGNAME}-* && cabal check)
+    rm -rf ./dist-newstyle
+
+    # haddock
+    if $HADDOCK; then cabal new-haddock -w ${HC} ${TEST} ${BENCH} all; else echo "Skipping haddock generation";fi
 }
 
 if [ x"$dirs" = x ]; then
