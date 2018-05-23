59268e16f977414c81e29e7ef1c1b68129e73b85
Kei Hibino <ex8k.hibino@gmail.com>
2018-05-23 13:11:46 +0900

    text-postgresql: simplify guard code of v6HostAddress.

--- text-postgresql/src/Data/PostgreSQL/NetworkAddress.hs
+++ text-postgresql/src/Data/PostgreSQL/NetworkAddress.hs
@@ -49,8 +49,9 @@ v6HostAddressLong = V6HostAddress
 v6HostAddress :: [Word16] -> [Word16] -> Maybe V6HostAddress
 v6HostAddress ls rs  = do
   let v6length = 8
-  guard . null . drop v6length $ ls ++ rs
-  [a, b, c, d, e, f, g, h]  <-  pure $ ls ++ replicate (v6length - length ls - length rs) 0 ++ rs
+      lrlength = length $ ls ++ rs
+  guard $ lrlength <= v6length
+  [a, b, c, d, e, f, g, h]  <-  pure $ ls ++ replicate (v6length - lrlength) 0 ++ rs
   pure $ v6HostAddressLong a b c d e f g h
 
 v6HostAddressR :: [Word16] -> Maybe V6HostAddress
