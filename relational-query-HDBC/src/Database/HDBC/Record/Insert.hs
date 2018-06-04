ff12ebeae1390c0f843065c0e5b67bc4db50165c
Kei Hibino <ex8k.hibino@gmail.com>
2018-06-04 17:52:47 +0900

    relational-query-HDBC: update copyright notice.

--- relational-query-HDBC/src/Database/HDBC/Record/Insert.hs
+++ relational-query-HDBC/src/Database/HDBC/Record/Insert.hs
@@ -2,7 +2,7 @@
 
 -- |
 -- Module      : Database.HDBC.Record.Insert
--- Copyright   : 2013 Kei Hibino
+-- Copyright   : 2013-2018 Kei Hibino
 -- License     : BSD3
 --
 -- Maintainer  : ex8k.hibino@gmail.com
