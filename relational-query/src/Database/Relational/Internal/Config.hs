fba1aade563cdc495c01d0154de38a5d7fdecf23
Kei Hibino <ex8k.hibino@gmail.com>
2018-06-22 19:12:31 +0900

    relational-query: hide NameConfig data constructor.

--- relational-query/src/Database/Relational/Internal/Config.hs
+++ relational-query/src/Database/Relational/Internal/Config.hs
@@ -27,8 +27,14 @@ module Database.Relational.Internal.Config (
   identifierQuotation,
   nameConfig,
 
-  NameConfig (..),
+  NameConfig,
   defaultNameConfig,
+
+  {- field labels of 'NameConfig' type.
+     To avoid haddock bug ( https://github.com/haskell/haddock/issues/456 ),
+     they are not listed in NameConfig ( ... ). -}
+  recordConfig,
+  relationVarName,
 
   ProductUnitSupport (..), SchemaNameMode (..), IdentifierQuotation (..),
   ) where
