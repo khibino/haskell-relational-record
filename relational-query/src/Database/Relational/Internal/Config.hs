2d649c0ac5118a76dd078971481107c5e5049a35
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-04 21:23:53 +0900

    relational-query: update copyright notice.

--- relational-query/src/Database/Relational/Internal/Config.hs
+++ relational-query/src/Database/Relational/Internal/Config.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      : Database.Relational.Internal.Config
--- Copyright   : 2017-2018 Kei Hibino
+-- Copyright   : 2017-2019 Kei Hibino
 -- License     : BSD3
 --
 -- Maintainer  : ex8k.hibino@gmail.com
