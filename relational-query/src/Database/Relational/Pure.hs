c11cb20d4fad508a5431f0a75e72feb24451362e
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-05 14:02:35 +0900

    relational-query: apply literal module.

--- relational-query/src/Database/Relational/Pure.hs
+++ relational-query/src/Database/Relational/Pure.hs
@@ -15,7 +15,6 @@
 module Database.Relational.Pure () where
 
 import Control.Applicative (pure)
-import Data.Monoid ((<>))
 import Data.Int (Int8, Int16, Int32, Int64)
 import Data.Word (Word8, Word16, Word32, Word64, Word)
 import Data.ByteString (ByteString)
@@ -26,8 +25,7 @@ import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LT
 import Text.Printf (PrintfArg, printf)
-import Data.Time (FormatTime, Day, TimeOfDay, LocalTime, formatTime)
-import Data.Time.Locale.Compat (defaultTimeLocale)
+import Data.Time (Day, TimeOfDay, LocalTime)
 import Data.DList (DList, fromList)
 
 import Language.SQL.Keyword (Keyword (..))
@@ -36,82 +34,84 @@ import Database.Record
 import Database.Record.Persistable
   (runPersistableRecordWidth)
 
-import Database.Relational.Internal.String (StringSQL, stringSQL, boolSQL)
+import Database.Relational.Internal.String
+  (StringSQL, stringSQL, boolSQL)
+import qualified Database.Relational.Internal.Literal as Lit
 
 import Database.Relational.ProjectableClass (LiteralSQL (..))
-import Database.Relational.Pure.Internal (intTermsSQL, stringTermsSQL, stringExprSQL)
+
 
 -- | Constant SQL terms of '()'.
 instance LiteralSQL ()
 
 -- | Constant SQL terms of 'Int8'.
 instance LiteralSQL Int8 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Int16'.
 instance LiteralSQL Int16 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Int32'.
 instance LiteralSQL Int32 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Int64'.
 instance LiteralSQL Int64 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Int'.
 --   Use this carefully, because this is architecture dependent size of integer type.
 instance LiteralSQL Int where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Word8'.
 instance LiteralSQL Word8 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Word16'.
 instance LiteralSQL Word16 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Word32'.
 instance LiteralSQL Word32 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Word64'.
 instance LiteralSQL Word64 where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'Word'.
 --   Use this carefully, because this is architecture dependent size of integer type.
 instance LiteralSQL Word where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 instance LiteralSQL Integer where
-  showLiteral' = intTermsSQL
+  showLiteral' = pure . Lit.integral
 
 -- | Constant SQL terms of 'String'.
 instance LiteralSQL String where
-  showLiteral' = stringTermsSQL
+  showLiteral' = pure . Lit.stringExpr
 
 -- | Constant SQL terms of 'ByteString'.
 instance LiteralSQL ByteString where
-  showLiteral' = stringTermsSQL . T.unpack . T.decodeUtf8
+  showLiteral' = pure . Lit.stringExpr . T.unpack . T.decodeUtf8
 
 -- | Constant SQL terms of 'LB.ByteString'.
 instance LiteralSQL LB.ByteString where
-  showLiteral' = stringTermsSQL . LT.unpack . LT.decodeUtf8
+  showLiteral' = pure . Lit.stringExpr . LT.unpack . LT.decodeUtf8
 
 -- | Constant SQL terms of 'Text'.
 instance LiteralSQL Text where
-  showLiteral' = stringTermsSQL . T.unpack
+  showLiteral' = pure . Lit.stringExpr . T.unpack
 
 -- | Constant SQL terms of 'LT.Text'.
 instance LiteralSQL LT.Text where
-  showLiteral' = stringTermsSQL . LT.unpack
+  showLiteral' = pure . Lit.stringExpr . LT.unpack
 
 -- | Constant SQL terms of 'Char'.
 instance LiteralSQL Char where
-  showLiteral' = stringTermsSQL . (:"")
+  showLiteral' = pure . Lit.stringExpr . (:"")
 
 -- | Constant SQL terms of 'Bool'.
 instance LiteralSQL Bool where
@@ -131,20 +131,17 @@ instance LiteralSQL Float where
 instance LiteralSQL Double where
   showLiteral' = floatTerms
 
-constantTimeTerms :: FormatTime t => Keyword -> String -> t -> DList StringSQL
-constantTimeTerms kw fmt t = pure $ kw <> stringExprSQL (formatTime defaultTimeLocale fmt t)
-
 -- | Constant SQL terms of 'Day'.
 instance LiteralSQL Day where
-  showLiteral' = constantTimeTerms DATE "%Y-%m-%d"
+  showLiteral' = pure . Lit.timestamp DATE "%Y-%m-%d"
 
 -- | Constant SQL terms of 'TimeOfDay'.
 instance LiteralSQL TimeOfDay where
-  showLiteral' = constantTimeTerms TIME "%H:%M:%S"
+  showLiteral' = pure . Lit.timestamp TIME "%H:%M:%S"
 
 -- | Constant SQL terms of 'LocalTime'.
 instance LiteralSQL LocalTime where
-  showLiteral' = constantTimeTerms TIMESTAMP "%Y-%m-%d %H:%M:%S"
+  showLiteral' = pure . Lit.timestamp TIMESTAMP "%Y-%m-%d %H:%M:%S"
 
 showMaybeTerms :: LiteralSQL a => PersistableRecordWidth a -> Maybe a -> DList StringSQL
 showMaybeTerms wa = d  where
--- relational-query/src/Database/Relational/Pure/NonStandard/TIMESTAMPTZ.hs
+++ relational-query/src/Database/Relational/Pure/NonStandard/TIMESTAMPTZ.hs
@@ -3,18 +3,22 @@
 
 module Database.Relational.Pure.NonStandard.TIMESTAMPTZ () where
 
+import Control.Applicative (pure)
 import Data.Time (UTCTime, ZonedTime)
-import Database.Relational.ProjectableClass (LiteralSQL (..))
-import Database.Relational.Pure.Internal (constantTimeTerms)
+
 import Language.SQL.Keyword (Keyword (..))
+
+import qualified Database.Relational.Internal.Literal as Lit
+
+import Database.Relational.ProjectableClass (LiteralSQL (..))
+
 
 -- | Constant SQL terms of 'ZonedTime'.
 --   This generates ***NOT STANDARD*** SQL of TIMESTAMPTZ literal.
 instance LiteralSQL ZonedTime where
-  showLiteral' = constantTimeTerms TIMESTAMPTZ "%Y-%m-%d %H:%M:%S%z"
+  showLiteral' = pure . Lit.timestamp TIMESTAMPTZ "%Y-%m-%d %H:%M:%S%z"
 
 -- | Constant SQL terms of 'UTCTime'.
 --   This generates ***NOT STANDARD*** SQL of TIMESTAMPTZ literal with UTC timezone.
 instance LiteralSQL UTCTime where
-  showLiteral' = constantTimeTerms TIMESTAMPTZ "%Y-%m-%d %H:%M:%S%z"
-
+  showLiteral' = pure . Lit.timestamp TIMESTAMPTZ "%Y-%m-%d %H:%M:%S%z"
