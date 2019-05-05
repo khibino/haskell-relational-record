29ac76378f1f9653269e87a5e81cb1621d05a378
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-05 13:58:08 +0900

    relational-query: add internal literal module.

---
+++ relational-query/src/Database/Relational/Internal/Literal.hs
@@ -0,0 +1,42 @@
+-- |
+-- Module      : Database.Relational.Internal.Literal
+-- Copyright   : 2019 Kei Hibino
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- This module provides definitions to lift haskell expressions
+-- into SQL expressions.
+module Database.Relational.Internal.Literal (
+  stringExpr,
+  integral,
+  timestamp,
+  ) where
+
+import Data.Time (FormatTime, formatTime)
+import Data.Time.Locale.Compat (defaultTimeLocale)
+
+import Language.SQL.Keyword (Keyword)
+
+import Database.Relational.Internal.String (StringSQL, stringSQL)
+
+
+-- | Escape 'String' for constant SQL string expression.
+escapeStringToSqlExpr :: String -> String
+escapeStringToSqlExpr = rec where
+  rec ""        = ""
+  rec ('\'':cs) = '\'' : '\'' : rec cs
+  rec (c:cs)    = c : rec cs
+
+-- | From 'String' into constant SQL string expression.
+stringExpr :: String -> StringSQL
+stringExpr = stringSQL . ('\'' :) . (++ "'") . escapeStringToSqlExpr
+
+-- | Constant integral SQL term.
+integral :: (Show a, Integral a) => a -> StringSQL
+integral = stringSQL . show
+
+timestamp :: FormatTime t => Keyword -> String -> t -> StringSQL
+timestamp kw fmt t = kw <> stringExpr (formatTime defaultTimeLocale fmt t)
