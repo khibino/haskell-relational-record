49c2866474f46634d2ae67e44bcba2422ed71d97
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-05 15:57:58 +0900

    relational-record-examples: compat with GHC 7.

--- relational-record-examples/src/specializedExamples.hs
+++ relational-record-examples/src/specializedExamples.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 {-# LANGUAGE MonadComprehensions #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TemplateHaskell #-}
