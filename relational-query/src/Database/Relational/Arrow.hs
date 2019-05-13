f4595c4d328ae3cdebeb1e4435a9378f652815d6
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-13 23:00:45 +0900

    relational-query: update copyright notice.

--- relational-query/src/Database/Relational/Arrow.hs
+++ relational-query/src/Database/Relational/Arrow.hs
@@ -3,7 +3,7 @@
 
 -- |
 -- Module      : Database.Relational.Arrow
--- Copyright   : 2015-2018 Kei Hibino
+-- Copyright   : 2015-2019 Kei Hibino
 -- License     : BSD3
 --
 -- Maintainer  : ex8k.hibino@gmail.com
