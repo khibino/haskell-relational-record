21288a1b60f63fcb3d857bb7e48d440035491e07
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-14 02:05:31 +0900

    relational-record-examples: apply custom API for SQLite3.

--- relational-record-examples/lib/Database/Relational/CustomSQLite3.hs
+++ relational-record-examples/lib/Database/Relational/CustomSQLite3.hs
@@ -7,7 +7,7 @@ module Database.Relational.CustomSQLite3 (
   module Database.HDBC.Session,
   module Database.HDBC.Sqlite3,
   module Database.Record,
-  module Database.Relational,
+  module Database.Custom.SQLite3,
 
   runRelation,
   makeRelationalRecord,
@@ -21,13 +21,13 @@ import Database.HDBC.Record hiding (execute, finish)
 import Database.HDBC.Session
 import Database.HDBC.Sqlite3
 import Database.Record hiding (unique)
-import Database.Relational hiding (unique)
+import Database.Custom.SQLite3
 
 runRelation :: (ToSql SqlValue p,
                 IConnection conn,
                 FromSql SqlValue a) =>
                conn -> Relation p a -> p -> IO [a]
-runRelation conn q p = runQuery conn (relationalQuery q) p
+runRelation conn q p = runQuery conn (relationalQuery q []) p
 
 makeRelationalRecord :: Name -> Q [Dec]
 makeRelationalRecord = makeRelationalRecord' defaultConfig
