6e3ebe7ba91c149d50af1e421cd41b311c6c79b7
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-15 01:13:13 +0900

    relational-schemas: fix warning message.

--- relational-schemas/src/Database/Relational/Schema/PgCatalog/PgAttribute.hs
+++ relational-schemas/src/Database/Relational/Schema/PgCatalog/PgAttribute.hs
@@ -1,5 +1,5 @@
 module Database.Relational.Schema.PgCatalog.PgAttribute
-  {-# DEPRECATED "import Database.Relational.Schema.PostgreSQL.PgType instead of this module." #-} (
+  {-# DEPRECATED "import Database.Relational.Schema.PostgreSQL.PgAttribute instead of this module." #-} (
   module Database.Relational.Schema.PostgreSQL.PgAttribute,
   ) where
 
