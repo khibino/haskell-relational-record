892bd17ca47f1e88b9ed05c7b66a3aac87dfad43
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-17 15:21:54 +0900

    relational-query: generalize type of internal guard.

--- relational-query/src/Database/Relational/SqlSyntax/Join.hs
+++ relational-query/src/Database/Relational/SqlSyntax/Join.hs
@@ -25,30 +25,30 @@ import Database.Relational.SqlSyntax.Types
 
 
 -- | Push new tree into product right term.
-growRight :: Maybe (Node (DList (Predicate Flat)))            -- ^ Current tree
-          -> (NodeAttr, ProductTree (DList (Predicate Flat))) -- ^ New tree to push into right
-          -> Node (DList (Predicate Flat))                    -- ^ Result node
+growRight :: Maybe (Node (DList g))            -- ^ Current tree
+          -> (NodeAttr, ProductTree (DList g)) -- ^ New tree to push into right
+          -> Node (DList g)                    -- ^ Result node
 growRight = d  where
   d Nothing  (naR, q) = Node naR q
   d (Just l) (naR, q) = Node Just' $ Join l (Node naR q) mempty
 
 -- | Push new leaf node into product right term.
-growProduct :: Maybe (Node (DList (Predicate Flat)))  -- ^ Current tree
+growProduct :: Maybe (Node (DList g))                 -- ^ Current tree
             -> (NodeAttr, (Bool, Qualified SubQuery)) -- ^ New leaf to push into right
-            -> Node (DList (Predicate Flat))          -- ^ Result node
+            -> Node (DList g)                         -- ^ Result node
 growProduct =  match  where
   match t (na, q) =  growRight t (na, Leaf q)
 
 -- | Add restriction into top product of product tree.
-restrictProduct' :: ProductTree (DList (Predicate Flat)) -- ^ Product to restrict
-                 -> Predicate Flat                       -- ^ Restriction to add
-                 -> ProductTree (DList (Predicate Flat)) -- ^ Result product
+restrictProduct' :: ProductTree (DList g) -- ^ Product to restrict
+                 -> g                     -- ^ Restriction to add
+                 -> ProductTree (DList g) -- ^ Result product
 restrictProduct' =  d  where
   d (Join lp rp rs) rs' = Join lp rp (rs <> pure rs')
   d leaf'@(Leaf _)         _   = leaf' -- or error on compile
 
 -- | Add restriction into top product of product tree node.
-restrictProduct :: Node (DList (Predicate Flat)) -- ^ Target node which has product to restrict
-                -> Predicate Flat                -- ^ Restriction to add
-                -> Node (DList (Predicate Flat)) -- ^ Result node
+restrictProduct :: Node (DList g) -- ^ Target node which has product to restrict
+                -> g              -- ^ Restriction to add
+                -> Node (DList g) -- ^ Result node
 restrictProduct (Node a t) e = Node a (restrictProduct' t e)
