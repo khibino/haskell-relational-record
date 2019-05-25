02397d22c8c29c811a1c2edbcec25fc9247f75b9
Kei Hibino <ex8k.hibino@gmail.com>
2019-05-25 14:07:54 +0900

    relational-query: orthogonality

--- relational-query/src/Database/Relational/Monad/Trans/Assigning.hs
+++ relational-query/src/Database/Relational/Monad/Trans/Assigning.hs
@@ -35,7 +35,7 @@ import Database.Relational.Internal.ContextType (Flat)
 import Database.Relational.SqlSyntax (Assignment)
 import Database.Relational.Typed.Table (Table)
 import qualified Database.Relational.Typed.Table as Table
-import Database.Relational.Typed.Record (Record, recordColumns)
+import Database.Relational.Typed.Record (Record, recordColumns, unsafeRecordFromColumns)
 
 import Database.Relational.Pi (Pi)
 import qualified Database.Relational.Record as Record
@@ -64,7 +64,7 @@ instance MonadQualify q m => MonadQualify q (Assignings r m) where
 type AssignTarget r v = Pi r v
 
 targetRecord :: AssignTarget r v ->  Table r -> Record Flat v
-targetRecord pi' tbl = Record.wpi (Table.recordWidth tbl) (Record.unsafeFromTable tbl) pi'
+targetRecord pi' tbl = Record.wpi (Table.recordWidth tbl) (unsafeRecordFromColumns $ Table.columns tbl) pi'
 
 -- | Add an assignment.
 assignTo :: Monad m => Record Flat v ->  AssignTarget r v -> Assignings r m ()
