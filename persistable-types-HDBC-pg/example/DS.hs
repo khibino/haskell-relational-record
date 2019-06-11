e7d67fa1a3be2c7aad57a219c2b3c4ff0b4872b7
Kei Hibino <ex8k.hibino@gmail.com>
2019-06-11 14:19:44 +0900

    persistable-types-HDBC-pg/example: make up-to-date.

--- persistable-types-HDBC-pg/example/DS.hs
+++ persistable-types-HDBC-pg/example/DS.hs
@@ -4,9 +4,9 @@ module DS (definePgTable) where
 
 import Language.Haskell.TH (Q, Dec, Name)
 import Database.HDBC.PostgreSQL (connectPostgreSQL)
-import Database.HDBC.Query.TH (defineTableFromDB)
-import Database.HDBC.Schema.Driver (Driver (..))
-import Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
+import Database.Relational.HDBC.TH (defineTableFromDB)
+import Database.Schema.HDBC.Driver (Driver (..))
+import Database.Schema.HDBC.PostgreSQL (driverPostgreSQL)
 import Data.PostgreSQL.NetworkAddress
 
 addNetAddress :: Driver conn -> Driver conn
--- persistable-types-HDBC-pg/example/InetExample.hs
+++ persistable-types-HDBC-pg/example/InetExample.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module SinglePKey where
 
+import GHC.Generics (Generic)
 import DS
 import Database.HDBC.PostgreSQL.Persistable ()
 
 $(definePgTable
   "EXAMPLE" "inet_example"
-  [''Eq, ''Show])
+  [''Eq, ''Show, ''Generic])
